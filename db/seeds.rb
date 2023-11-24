64c2c288f60c2b8c9114d43f9515b5b1eefcdd32
Keith Maibach <keith@laterolabs.ca>
2023-11-24 15:46:19 -0800

    add navigate and user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(email: "user@laterolabs.com", password: "password")
+
+User.create(email: "admin@example.com", password: "password")
