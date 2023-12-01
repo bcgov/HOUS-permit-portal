86113688fe5bb0186867dba74445204f9f402954
Keith Maibach <keith@laterolabs.ca>
2023-12-01 11:28:09 -0800

    add custom toasts

--- app/frontend/components/shared/flash-message.tsx
+++ app/frontend/components/shared/flash-message.tsx
@@ -1,6 +1,13 @@
-import { ToastPositionWithLogical, useToast } from "@chakra-ui/react"
+import { Box, Flex, Heading, Text, ToastPosition, ToastProps, useToast } from "@chakra-ui/react"
+import {
+  faCircleCheck,
+  faCircleExclamation,
+  faCircleInfo,
+  faTriangleExclamation,
+} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { observer } from "mobx-react-lite"
-import { useEffect } from "react"
+import React, { useEffect } from "react"
 import { useMst } from "../../setup/root"
 
 export const FlashMessage = observer(() => {
@@ -10,9 +17,48 @@ export const FlashMessage = observer(() => {
 
   useEffect(() => {
     if (isVisible) {
-      toast({ title, description, status, isClosable, duration, position: position as ToastPositionWithLogical })
+      toast({
+        title,
+        description,
+        status,
+        isClosable,
+        duration,
+        position: position as ToastPosition,
+        render: (props) => <CustomToast {...props} />,
+      })
     }
   }, [isVisible, status, title, description])
 
   return null
 })
+
+const CustomToast = ({ title, description, status }: ToastProps) => {
+  const iconMap = {
+    success: faCircleCheck,
+    warning: faTriangleExclamation,
+    error: faCircleExclamation,
+    info: faCircleInfo,
+  }
+
+  return (
+    <Flex
+      direction="column"
+      gap={2}
+      bg={`semantic.${status}Light`}
+      border="1px solid"
+      borderRadius="lg"
+      borderColor={`semantic.${status}`}
+      p={4}
+    >
+      <Flex align="flex-start" gap={2}>
+        <Box color={`semantic.${status}`}>
+          <FontAwesomeIcon style={{ height: 20, width: 20 }} icon={iconMap[status]} />
+        </Box>
+        <Flex direction="column" gap={2}>
+          {title && <Heading fontSize="md">{title}</Heading>}
+          {description && <Text>{description}</Text>}
+        </Flex>
+      </Flex>
+    </Flex>
+  )
+}
