032f430846fb77c2797f7ba6d0f4069d8ee90444
Keith Maibach <keith@laterolabs.ca>
2023-12-12 13:27:30 -0800

    add has_many contacts to local jurisdicitons

--- app/models/local_jurisdiction.rb
+++ app/models/local_jurisdiction.rb
@@ -1,6 +1,7 @@
 class LocalJurisdiction < ApplicationRecord
   # Associations
   has_many :permit_applications
+  has_many :contacts
   has_many :review_managers, -> { where(role: User.roles[:review_manager]) }, class_name: "User"
   has_many :reviewers, -> { where(role: User.roles[:reviewer]) }, class_name: "User"
 
