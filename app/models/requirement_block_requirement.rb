a3703d1781c6d9664bc82fc84f7726cb63b7017c
Tawshif Alam <tawshif@laterolabs.com>
2023-12-15 15:52:44 -0800

    [sc-67046] Add requirement related model factories, and add model specs for requirement_block and requirement_block_requirement

--- app/models/requirement_block_requirement.rb
+++ app/models/requirement_block_requirement.rb
@@ -1,4 +1,8 @@
 class RequirementBlockRequirement < ApplicationRecord
   belongs_to :requirement
   belongs_to :requirement_block
+
+  acts_as_list scope: :requirement_block, top_of_list: 0
+
+  validates_uniqueness_of :requirement, scope: :requirement_block
 end
