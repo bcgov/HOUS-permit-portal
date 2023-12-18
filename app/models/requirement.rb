fe8cdc2cdda1755250cd6341e67a9fbfe70b4ca1
Tawshif Alam <tawshif@laterolabs.com>
2023-12-18 15:24:26 -0800

    [sc-67046] Add requirement model initial validations and specs

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -1,5 +1,25 @@
 class Requirement < ApplicationRecord
-  has_many :requirement_block_requirements
+  has_many :requirement_block_requirements, dependent: :destroy
+  has_many :requirement_blocks, through: :requirement_block_requirements
 
-  enum input_type: { text: 0, number: 1, checkbox: 2, select: 3, multi_option_select: 4, date: 5 }, _prefix: :input_type
+  enum input_type: { text: 0, number: 1, checkbox: 2, select: 3, multi_option_select: 4, date: 5 }, _prefix: true
+
+  validate :validate_options_for_select_inputs
+
+  def value_options
+    return nil if input_options.blank? || input_options["value_options"].blank?
+
+    input_options["value_options"]
+  end
+
+  private
+
+  def validate_options_for_select_inputs
+    return unless input_type_select? || input_type_multi_option_select?
+
+    if input_options.blank? || input_options["value_options"].blank? || !input_options["value_options"].is_a?(Array) ||
+         !input_options["value_options"].all? { |option| option.is_a?(String) }
+      errors.add(:input_options, "select inputs must have options defined")
+    end
+  end
 end
--- app/models/requirement_block.rb
+++ app/models/requirement_block.rb
@@ -1,5 +1,5 @@
 class RequirementBlock < ApplicationRecord
-  has_many :requirement_block_requirements, -> { order(position: :asc) }
+  has_many :requirement_block_requirements, -> { order(position: :asc) }, dependent: :destroy
   has_many :requirements, through: :requirement_block_requirements
 
   enum sign_off_role: { any: 0 }, _prefix: true
