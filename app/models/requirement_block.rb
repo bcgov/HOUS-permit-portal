f14b9b8e6203d78436e72b20c5fc743f8d9b9e55
Tawshif Alam <tawshif@laterolabs.com>
2024-01-03 09:48:07 -0800

    [sc-67046] Add to form json method for requirements block and add corresponding spec

--- app/models/requirement_block.rb
+++ app/models/requirement_block.rb
@@ -4,4 +4,16 @@ class RequirementBlock < ApplicationRecord
 
   enum sign_off_role: { any: 0 }, _prefix: true
   enum reviewer_role: { any: 0 }, _prefix: true
+
+  def to_form_json
+    {
+      id: id,
+      legend: name,
+      key: "fieldSet#{id}",
+      label: name,
+      input: false,
+      tableView: false,
+      components: requirements.map(&:to_form_json),
+    }
+  end
 end
