9ed053b239da4f902023f065a45c7c44d184b238
Tawshif Alam <tawshif@laterolabs.com>
2024-01-23 14:32:11 -0800

    [bphh-385] Update migration to destroy all requirement blocks for clean data

--- db/migrate/20240123185911_change_requirement_and_requirement_block_relationship.rb
+++ db/migrate/20240123185911_change_requirement_and_requirement_block_relationship.rb
@@ -3,6 +3,7 @@ class ChangeRequirementAndRequirementBlockRelationship < ActiveRecord::Migration
     # removes all references from join table
     execute("DELETE FROM requirement_block_requirements")
     Requirement.destroy_all
+    RequirementBlock.destroy_all
 
     # add requirement block reference to requirement to make it a 1 to many relationship
     add_reference :requirements, :requirement_block, null: false, foreign_key: true, type: :uuid
