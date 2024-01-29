9d50c2bf3bc3fb5b11c3aaa6ee0c035d3e8046b4
Sunny To <sunny@laterolabs.com>
2024-01-29 09:08:17 -0800

    inital shrine / form file upload setup test

--- app/frontend/components/shared/chefs/custom-formio-components/index.d.ts
+++ app/frontend/components/shared/chefs/custom-formio-components/index.d.ts
@@ -61,6 +61,27 @@ declare const _default: {
         }
         title: string
       }
+      s3custom: {
+        (formio: any): {
+          title: string
+          name: string
+          uploadFile(
+            file: any,
+            fileName: any,
+            dir: any,
+            progressCallback: any,
+            url: any,
+            options: any,
+            fileKey: any,
+            groupPermissions: any,
+            groupId: any,
+            abortCallback: any
+          ): any
+          deleteFile(fileInfo: any): any
+          downloadFile(file: any): any
+        }
+        title: string
+      }
     }
   }
 }
--- app/frontend/components/shared/chefs/custom-formio-components/providers/storage/index.js
+++ app/frontend/components/shared/chefs/custom-formio-components/providers/storage/index.js
@@ -1,4 +1,6 @@
 import chefs from "./chefs.js"
+import s3custom from "./s3custom.js"
 export default {
   chefs,
+  s3custom,
 }
