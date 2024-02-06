2cef5bd89c24aa478ddaeb65e33847383c996d16
Keith Maibach <keith@laterolabs.ca>
2024-02-06 11:14:31 -0800

    remove some error constants

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -26,10 +26,6 @@ class Api::ApplicationController < ActionController::API
   end
 
   def user_not_authorized(exception)
-    render_error Constants::Error::USER_NOT_AUTHORIZED_ERROR,
-                 "misc.user_not_authorized_error",
-                 message_opts: {
-                   error_message: exception.message,
-                 } and return
+    render_error "misc.user_not_authorized_error", message_opts: { error_message: exception.message } and return
   end
 end
