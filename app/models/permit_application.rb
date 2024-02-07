57843bffe64d3841099ce9483cfeb37a9fd8a872
Keith Maibach <keith@laterolabs.ca>
2024-02-07 14:35:28 -0800

    add unique permit application numbers

--- app/models/permit_application.rb
+++ app/models/permit_application.rb
@@ -1,5 +1,6 @@
 class PermitApplication < ApplicationRecord
-  searchkick searchable: %i[reverse_qualified_name], word_start: %i[reverse_qualified_name]
+  searchkick searchable: %i[reverse_qualified_name],
+             word_start: %i[reverse_qualified_name]
 
   belongs_to :submitter, class_name: "User"
   belongs_to :jurisdiction
@@ -25,9 +26,11 @@ class PermitApplication < ApplicationRecord
       updated_at: updated_at,
       review_managers_size: review_managers_size,
       reviewers_size: reviewers_size,
-      permit_applications_size: permit_applications_size,
+      permit_applications_size: permit_applications_size
     }
   end
+
+  before_create :assign_unique_number
 
   #stubs for UI
   alias number id
@@ -38,13 +41,51 @@ class PermitApplication < ApplicationRecord
   def requirements
     #TODO: add versioning for requirement templates, etc.  for now just stub the return of the requirement template to use and its form data
     #need to look up jurisidcitional version and enablement as well
-    requirement_template = RequirementTemplate.find_by(activity: activity, permit_type: permit_type)
+    requirement_template =
+      RequirementTemplate.find_by(activity: activity, permit_type: permit_type)
     requirement_template ? requirement_template.to_form_json : nil
+  end
+
+  def number_prefix
+    jurisdiction.prefix
+  end
+
+  def assign_unique_number
+    last_number =
+      jurisdiction
+        .permit_applications
+        .where("number LIKE ?", "#{number_prefix}-%")
+        .order(Arel.sql("LENGTH(number) DESC"), number: :desc)
+        .limit(1)
+        .pluck(:number)
+        .first
+
+    if last_number
+      number_parts = last_number.split("-")
+      sequence_part = number_parts[1..-1].join.to_i + 1 # Increment the sequence
+      new_number =
+        format(
+          "%s-%03d-%03d-%03d",
+          number_prefix,
+          sequence_part / 1_000_000 % 1000,
+          sequence_part / 1000 % 1000,
+          sequence_part % 1000
+        )
+    else
+      # Start with the initial number if there are no previous numbers
+      new_number = format("%s-001-000-000", number_prefix)
+    end
+
+    # Assign the new number to the permit application
+    self.number = new_number
+    return new_number
   end
 
   private
 
   def submitter_must_have_role
-    errors.add(:submitter, "must have the submitter role") unless submitter&.submitter?
+    unless submitter&.submitter?
+      errors.add(:submitter, "must have the submitter role")
+    end
   end
 end
---
+++ db/migrate/20240207194117_add_number_to_permit_applications.rb
@@ -0,0 +1,9 @@
+class AddNumberToPermitApplications < ActiveRecord::Migration[7.1]
+  def change
+    add_column :jurisdictions, :prefix, :string, null: false
+    add_column :permit_applications, :number, :string
+
+    add_index :jurisdictions, :prefix, unique: true
+    add_index :permit_applications, :number, unique: true
+  end
+end
