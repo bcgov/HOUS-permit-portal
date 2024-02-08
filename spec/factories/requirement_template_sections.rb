a28b1e3e15c4b28b3a1dab988afaa3acde7077ae
Keith Maibach <keith@laterolabs.ca>
2024-02-08 10:00:51 -0800

    improve some specs

--- spec/factories/requirement_template_sections.rb
+++ spec/factories/requirement_template_sections.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :requirement_template_section do
     name { "MyString" }
-    code { "MyString" }
     requirement_template { nil }
     position { 1 }
+
+    initialize_with { new(attributes).tap { |section| section.id = attributes[:id] || SecureRandom.uuid } }
   end
 end
