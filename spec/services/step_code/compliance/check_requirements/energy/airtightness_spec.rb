f9b6aca6537004fae0fa7974291ed164095aa6a6
laurel <laurel@laterolabs.com>
2024-02-12 14:01:27 -0800

    Add specs for step code compliance
    [BPHH-546]

---
+++ spec/services/step_code/compliance/check_requirements/energy/airtightness_spec.rb
@@ -0,0 +1,68 @@
+RSpec.describe StepCode::Compliance::CheckRequirements::Energy::Airtightness do
+  let(:step) { 3 }
+  let!(:step_code) { create(:step_code, data_entries_attributes:) }
+  subject(:compliance_checker) do
+    StepCode::Compliance::CheckRequirements::Energy::Airtightness.new(
+      checklist: step_code.pre_construction_checklist,
+      step: step,
+    )
+  end
+
+  let(:ach_requirement) { 55 }
+  let(:nla_requirement) { 40 }
+  let(:nlr_requirement) { 35 }
+
+  before :each do
+    allow(subject).to receive(:ach_requirement) { ach_requirement }
+    allow(subject).to receive(:nla_requirement) { nla_requirement }
+    allow(subject).to receive(:nlr_requirement) { nlr_requirement }
+  end
+
+  context "when total ACH meets the energy step requirement" do
+    let(:data_entries_attributes) { [{ stage: :proposed, ach: 22 }, { stage: :proposed, ach: 22 }] }
+
+    it_behaves_like PASSING_STEP_CODE
+  end
+
+  context "when total NLA meets the energy step requirement" do
+    let(:data_entries_attributes) { [{ stage: :proposed, ach: 32, nla: 18 }, { stage: :proposed, ach: 32, nla: 18 }] }
+
+    it_behaves_like PASSING_STEP_CODE
+  end
+
+  context "when NLR meets the energy step requirement" do
+    let(:data_entries_attributes) do
+      [
+        {
+          stage: :proposed,
+          ach: 52,
+          nla: 42,
+          above_grade_heated_floor_area: 117.9,
+          below_grade_heated_floor_area: 109.9,
+          building_volume: 624.9,
+          building_envelope_surface_area: 517.40,
+        },
+      ]
+    end
+
+    it_behaves_like PASSING_STEP_CODE
+  end
+
+  context "when ACH, NLA, and NLR all do not meet energy step requirement" do
+    let(:data_entries_attributes) do
+      [
+        {
+          stage: :proposed,
+          ach: 72,
+          nla: 42,
+          above_grade_heated_floor_area: 117.9,
+          below_grade_heated_floor_area: 109.9,
+          building_volume: 1624.9,
+          building_envelope_surface_area: 517.40,
+        },
+      ]
+    end
+
+    it_behaves_like FAILED_STEP_CODE
+  end
+end
---
+++ spec/services/step_code/compliance/check_requirements/zero_carbon/prescriptive_spec.rb
@@ -0,0 +1,96 @@
+RSpec.describe StepCode::Compliance::CheckRequirements::ZeroCarbon::Prescriptive do
+  let(:step) { 3 }
+  let!(:step_code) { create(:step_code, data_entries_attributes:) }
+  subject(:compliance_checker) do
+    StepCode::Compliance::CheckRequirements::ZeroCarbon::Prescriptive.new(
+      checklist: step_code.pre_construction_checklist,
+      step: step,
+    )
+  end
+
+  let(:heating_requirement) { :zero_carbon }
+  let(:hot_water_requirement) { :zero_carbon }
+  let(:other_requirement) { :zero_carbon }
+
+  before :each do
+    allow(subject).to receive(:heating_requirement) { heating_requirement }
+    allow(subject).to receive(:hot_water_requirement) { hot_water_requirement }
+    allow(subject).to receive(:other_requirement) { other_requirement }
+  end
+
+  context "when heating requirement is not met" do
+    let(:data_entries_attributes) do
+      [
+        { stage: :proposed, heating_furnace: 0.9, heating_boiler: 0.2, heating_combo: 1.1 },
+        { stage: :proposed, heating_furnace: 0.9, heating_boiler: 0, heating_combo: 0.9 },
+      ]
+    end
+
+    it_behaves_like FAILED_STEP_CODE
+  end
+
+  context "when hot water requirement is not met" do
+    let(:data_entries_attributes) do
+      [
+        { stage: :proposed, heating_furnace: 0.9, heating_boiler: 0, heating_combo: 0.9, hot_water: 0.8 },
+        { stage: :proposed, heating_furnace: 0.9, heating_boiler: 0, heating_combo: 0.9, hot_water: 0.5 },
+      ]
+    end
+
+    it_behaves_like FAILED_STEP_CODE
+  end
+
+  context "when other requirement is not met" do
+    let(:data_entries_attributes) do
+      [
+        {
+          stage: :proposed,
+          heating_furnace: 0.9,
+          heating_boiler: 0,
+          heating_combo: 0.9,
+          hot_water: 0.8,
+          cooking: 0.2,
+          laundry: 0.6,
+        },
+        {
+          stage: :proposed,
+          heating_furnace: 0.9,
+          heating_boiler: 0,
+          heating_combo: 0.9,
+          hot_water: 0.5,
+          cooking: 0.4,
+          laundry: 0.7,
+        },
+      ]
+    end
+
+    it_behaves_like FAILED_STEP_CODE
+  end
+
+  context "when heating, hot water, and other all meet the requirement" do
+    let(:data_entries_attributes) do
+      [
+        {
+          stage: :proposed,
+          heating_furnace: 0.9,
+          heating_boiler: 0,
+          heating_combo: 0.9,
+          hot_water: 0.4,
+          cooking: 0.2,
+          laundry: 0.4,
+        },
+        {
+          stage: :proposed,
+          heating_furnace: 0.9,
+          heating_boiler: 0,
+          heating_combo: 0.9,
+          hot_water: 0.5,
+          cooking: 0.2,
+          laundry: 0.4,
+        },
+      ]
+    end
+
+    it_behaves_like PASSING_STEP_CODE
+  end
+end
