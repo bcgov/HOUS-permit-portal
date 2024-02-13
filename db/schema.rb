079e34b4078231e1499c53db00c4e7ace92dc57d
Sunny To <sunny@laterolabs.com>
2024-02-12 21:41:17 -0800

    remove alr validator until we need it later

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_08_011114) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_10_010209) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
