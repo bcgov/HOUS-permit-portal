283cdae93e6f001f2443567bdad29de1fb7a5d95
Derek Yau <derek@laterolabs.com>
2024-02-16 15:44:09 -0800

    update some limits and change routes to add in Sidekiq Username and password
    (cherry picked from commit 09c506fa9003e851a5b71e644b036ec0fa5448b6)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,19 @@ require "sidekiq/web"
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  if Rails.env.production?
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      ActiveSupport::SecurityUtils.secure_compare(
+        ::Digest::SHA256.hexdigest(username),
+        ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME']),
+      ) &
+        ActiveSupport::SecurityUtils.secure_compare(
+          ::Digest::SHA256.hexdigest(password),
+          ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']),
+        )
+    end
+  end
+
   mount Sidekiq::Web => "/sidekiq"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
