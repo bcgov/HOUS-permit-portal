3e92d97faec93e20d672a553fa899acac92992f5
Sunny To <sunny@laterolabs.com>
2024-02-21 14:56:31 -0800

    regex fix for section

--- app/frontend/utils/index.ts
+++ app/frontend/utils/index.ts
@@ -3,16 +3,19 @@ import * as R from "ramda"
 
 const SUBMISSION_DATA_PREFIX = "formSubmissionData"
 const FORMIO_SECTION_REGEX = /^section[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+const SECTION_COMPLETION = "section-completion-key"
 
 export const camelizeResponse = (data: { [key: string]: any }) => {
   return humps.camelizeKeys(data, function (key, convert) {
-    return key.startsWith(SUBMISSION_DATA_PREFIX) || FORMIO_SECTION_REGEX.test(key) ? key : convert(key)
+    return key == SECTION_COMPLETION || key.startsWith(SUBMISSION_DATA_PREFIX) || FORMIO_SECTION_REGEX.test(key)
+      ? key
+      : convert(key)
   })
 }
 
 export const decamelizeRequest = (params: { [key: string]: any }) => {
   return humps.decamelizeKeys(params, function (key, convert, options) {
-    return key.startsWith(SUBMISSION_DATA_PREFIX) ? key : convert(key)
+    return key == SECTION_COMPLETION || key.startsWith(SUBMISSION_DATA_PREFIX) ? key : convert(key)
   })
 }
 
