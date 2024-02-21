a1e8cdae171c637e8dbe38d3bab49caaa8feb07a
Sunny To <sunny@laterolabs.com>
2024-02-21 14:33:36 -0800

    navigation update for permit applications

--- app/frontend/components/domains/home/index.tsx
+++ app/frontend/components/domains/home/index.tsx
@@ -1,9 +1,8 @@
 import { observer } from "mobx-react-lite"
-import React, { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
+import React from "react"
 import { useMst } from "../../../setup/root"
 import { EUserRoles } from "../../../types/enums"
-import { SharedSpinner } from "../../shared/base/shared-spinner"
+import { PermitApplicationIndexScreen } from "../permit-application"
 import { ReviewManagerHomeScreen } from "./review-manager-home-screen"
 import { ReviewerHomeScreen } from "./reviewer-home-screen"
 import { SuperAdminHomeScreen } from "./super-admin-home-screen"
@@ -13,19 +12,13 @@ const roleSpecificScreens = (role: EUserRoles, props: IHomeScreenProps) => {
     [EUserRoles.superAdmin]: <SuperAdminHomeScreen {...props} />,
     [EUserRoles.reviewer]: <ReviewerHomeScreen {...props} />,
     [EUserRoles.reviewManager]: <ReviewManagerHomeScreen {...props} />,
-    [EUserRoles.submitter]: <SharedSpinner />,
+    [EUserRoles.submitter]: <PermitApplicationIndexScreen {...props} />,
   }[role]
 }
 
 export interface IHomeScreenProps {}
 
 export const HomeScreen = observer(({ ...props }: IHomeScreenProps) => {
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    if (currentUser.role === EUserRoles.submitter) navigate("/permit-applications")
-  }, [])
-
   const { userStore } = useMst()
   const { currentUser } = userStore
 
