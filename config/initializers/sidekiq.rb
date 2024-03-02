93c3c1436e994c30d53a1ad53ad737b499a06945
Derek Yau <derek@laterolabs.com>
2024-03-01 17:03:28 -0800

    Setup Sidekiq to use Redis Sentinel in production

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,19 @@
+require "sidekiq-cron"
+
+if ENV["DOCKER_BUILDING"].blank? # skip this during precompilation in the docker build stage
+  if Rails.env.production?
+    # in production mode we use redis-sentinel for HA Redis, locally / test just fallback to default
+    redis_cfg = {
+      url: ENV["REDIS_SENTINEL_MASTER_SET_URL"],
+      sentinels: [{ host: ENV["REDIS_SENTINEL_HOST"], port: (ENV["REDIS_SENTINEL_PORT"] || 26_379) }],
+      role: :master,
+    }
+
+    Sidekiq.configure_server { |config| config.redis = redis_cfg }
+    Sidekiq.configure_client { |config| config.redis = redis_cfg }
+
+    # Don't load crons in test and dev mode
+    schedule_file = "config/sidekiq_cron_schedule.yml"
+    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file)
+  end
+end
