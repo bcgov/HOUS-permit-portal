a2ed193eb71a9753eebc4fc44a6d6bc29779e004
Derek Yau <derek@laterolabs.com>
2024-03-01 22:44:17 -0800

    URL configuration is not getting picked up for some reason, use name and db keys explicitly instead

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,8 +4,12 @@ if ENV["SKIP_DEPENDENCY_INITIALIZERS"].blank? # skip this during precompilation 
   if Rails.env.production?
     # in production mode we use redis-sentinel for HA Redis, locally / test just fallback to default
     redis_cfg = {
-      url: ENV["REDIS_SENTINEL_MASTER_SET_URL"],
-      sentinels: [{ host: ENV["REDIS_SENTINEL_HOST"], port: (ENV["REDIS_SENTINEL_PORT"]&.to_i || 26_379) }],
+      name: ENV["REDIS_SENTINEL_MASTER_SET_NAME"],
+      sentinels:
+        Resolv
+          .getaddresses(ENV["REDIS_SENTINEL_HEADLESS"])
+          .map { |address| { host: address, port: (ENV["REDIS_SENTINEL_PORT"]&.to_i || 26_379) } },
+      db: ENV["SIDEKIQ_REDIS_DB"]&.to_i || 0,
       role: :master,
     }
 
@@ -14,10 +18,11 @@ if ENV["SKIP_DEPENDENCY_INITIALIZERS"].blank? # skip this during precompilation 
       config.queues = %w[promote_asset_and_make_derivatives default]
       config.concurrency = ENV["SIDEKIQ_CONCURRENCY"].to_i
     end
+
     Sidekiq.configure_client { |config| config.redis = redis_cfg }
 
     # # Don't load crons in test and dev mode
-    # schedule_file = "config/sidekiq_cron_schedule.yml"
-    # Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file)
+    schedule_file = "config/sidekiq_cron_schedule.yml"
+    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file)
   end
 end
