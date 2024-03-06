d0ff447d701c71995119a720126ea035a40336fd
Derek Yau <derek@laterolabs.com>
2024-03-06 13:10:38 -0800

    Fix consigno ENV vars

--- devops/docker/consigno-verifio-notarius-server/entrypoint.sh
+++ devops/docker/consigno-verifio-notarius-server/entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-# Start the CSA service
+
+set -euo pipefail 
+
+VAULT_SECRETS_DIR=/vault/secrets
+
+if [ -d ${VAULT_SECRETS_DIR} ]; then
+  set -a # enable mark variables which are modified or created for export
+  for i in ${VAULT_SECRETS_DIR}/*.env; do
+    echo "[entrypoint] Adding environment variables from ${i}"
+    source ${i}
+  done
+  set +a # disable mark variables which are modified or created for export
+else
+  echo "[entrypoint] Vault secrets directory (${VAULT_SECRETS_DIR}) does not exist"
+fi
+
+# exec "$@"
 csa-service start
 
 # Prevent the openshift container from "Completing"
