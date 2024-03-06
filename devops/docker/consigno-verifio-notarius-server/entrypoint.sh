d905b66ade5dbd66e0e0cf08cf820a24ff0e5f6e
Derek Yau <derek@laterolabs.com>
2024-03-06 14:17:32 -0800

    Modify Dockerfile and entrypoint for Consigno server

--- devops/docker/consigno-verifio-notarius-server/entrypoint.sh
+++ devops/docker/consigno-verifio-notarius-server/entrypoint.sh
@@ -15,8 +15,4 @@ else
   echo "[entrypoint] Vault secrets directory (${VAULT_SECRETS_DIR}) does not exist"
 fi
 
-# exec "$@"
-csa-service start
-
-# Prevent the openshift container from "Completing"
-tail -f /dev/null
+exec $CSA_HOME/jre/bin/java -DCSA_WEBSERVICES_ENABLED=true -Djava.library.path=$CSA_HOME/dll -Dfile.encoding=UTF-8 -Xmx1024m -cp $CSA_HOME/addons/*:$CSA_HOME/conf/:$CSA_HOME/sdk-components/*:$CSA_HOME/sdk-deps/* com.notarius.consigno.sdk.cli.Main -server /tmp
