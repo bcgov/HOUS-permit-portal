64264eb4568586eef6e3194038f2bd396ca460e8
Tawshif Alam <tawshif@laterolabs.com>
2024-03-08 14:48:21 -0800

    [bphh-635] Refactor requirement-field-edit sub components to individual files and organize them under folder

---
+++ app/frontend/components/domains/requirements-library/requirement-field-edit/editable-group.tsx
@@ -0,0 +1,41 @@
+import { Stack } from "@chakra-ui/react"
+import React from "react"
+import { FieldValues } from "react-hook-form"
+import { EditableHelperText, TEditableHelperTextProps } from "./editable-helper-text"
+import { EditableLabel, TEditableLabelProps } from "./editable-label"
+import { IsElectiveCheckbox, TIsElectiveCheckboxProps } from "./is-elective-checkbox"
+import { IsOptionalCheckbox, TIsOptionalCheckboxProps } from "./is-optional-checkbox"
+
+export type TEditableGroupProps<TFieldValues extends FieldValues> = {
+  editableLabelProps?: TEditableLabelProps<TFieldValues>
+  editableHelperTextProps?: TEditableHelperTextProps<TFieldValues>
+  isOptionalCheckboxProps: TIsOptionalCheckboxProps<TFieldValues>
+  isElectiveCheckboxProps: TIsElectiveCheckboxProps<TFieldValues>
+  editableInput?: JSX.Element
+  multiOptionEditableInput?: JSX.Element
+}
+
+export function EditableGroup<TFieldValues>({
+  editableLabelProps,
+  editableHelperTextProps,
+  editableInput,
+  multiOptionEditableInput,
+  isOptionalCheckboxProps,
+  isElectiveCheckboxProps,
+}: TEditableGroupProps<TFieldValues>) {
+  return (
+    <Stack spacing={4}>
+      <EditableLabel {...editableLabelProps} />
+      {editableInput}
+      {editableInput && <EditableHelperText {...editableHelperTextProps} />}
+      {multiOptionEditableInput && (
+        <Stack>
+          {multiOptionEditableInput}
+          <EditableHelperText {...editableHelperTextProps} />
+        </Stack>
+      )}
+      <IsOptionalCheckbox {...isOptionalCheckboxProps} />
+      <IsElectiveCheckbox mt={"0.625rem"} {...isElectiveCheckboxProps} />
+    </Stack>
+  )
+}
