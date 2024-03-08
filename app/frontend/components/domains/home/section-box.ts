fd83dc64e5b8330dc9853464d48e3445ed18f55f
laurel <laurel@laterolabs.com>
2024-03-08 11:27:58 -0800

    Add Review Manager Configuration Management Screen
    [BPHH-790]
    
    Replace content management (jurisdiction's about page) in review manager
    home screen with jurisdiction management screen. This screen links to the
    jurisdiction's about page.

---
+++ app/frontend/components/domains/home/section-box.ts
@@ -0,0 +1,11 @@
+import { Box, styled } from "@chakra-ui/react"
+
+export const SectionBox = styled(Box)
+SectionBox.defaultProps = {
+  as: "section",
+  borderRadius: "lg",
+  borderWidth: 1,
+  borderColor: "border.light",
+  p: 6,
+  w: "full",
+}
