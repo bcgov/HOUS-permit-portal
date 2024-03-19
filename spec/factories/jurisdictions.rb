7d4d692f0e46e6ec97cae611782780ba5bbe00cd
Keith Maibach <keith@laterolabs.ca>
2024-03-19 15:39:13 -0700

    add factories to fix specs

--- spec/factories/jurisdictions.rb
+++ spec/factories/jurisdictions.rb
@@ -7,5 +7,12 @@ FactoryBot.define do
     checklist_html { "<p>Some checklist</p>" }
     look_out_html { "<p>Some lookout</p>" }
     contact_summary_html { "<p>Some lookout</p>" }
+
+    after(:create) do |jurisdiction|
+      create(:permit_type_submission_contact, jurisdiction: jurisdiction)
+      # This creates a PermitTypeSubmissionContact associated with the jurisdiction.
+      # If your PermitTypeSubmissionContact factory requires a permit_type,
+      # ensure that a permit_type factory is also defined and associated here.
+    end
   end
 end
