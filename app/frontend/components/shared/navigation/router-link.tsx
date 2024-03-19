6cdce4bad844dd6f7a1541007125683fa3335e79
Keith Maibach <keith@laterolabs.ca>
2024-03-19 11:21:49 -0700

    add scroll to top for routerlinks

--- app/frontend/components/shared/navigation/router-link.tsx
+++ app/frontend/components/shared/navigation/router-link.tsx
@@ -1,12 +1,24 @@
 import { Link } from "@chakra-ui/react"
 import React, { forwardRef } from "react"
 import { LinkProps, Link as ReactRouterLink } from "react-router-dom"
+import { handleScrollToTop } from "../../../utils/utility-functions"
 
-interface IRouterLinkProps extends LinkProps {}
+interface IRouterLinkProps extends LinkProps {
+  onClick?: () => void
+}
 
-export const RouterLink = forwardRef<HTMLAnchorElement, IRouterLinkProps>(({ to, children, ...rest }, ref) => {
+export const RouterLink = forwardRef<HTMLAnchorElement, IRouterLinkProps>(({ to, children, onClick, ...rest }, ref) => {
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    // If there's an onClick prop passed, call it
+    if (onClick) {
+      onClick()
+    }
+
+    handleScrollToTop("outerScrollContainer")
+  }
+
   return (
-    <Link as={ReactRouterLink} to={to} ref={ref} {...rest}>
+    <Link as={ReactRouterLink} to={to} ref={ref} onClick={handleClick} {...rest}>
       {children}
     </Link>
   )
