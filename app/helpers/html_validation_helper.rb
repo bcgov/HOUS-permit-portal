82bbdb5a0bd350cac32c5bc98e4bf532611a4dfa
Sunny To <sunny@laterolabs.com>
2024-04-04 09:20:43 -0700

    remove comments

--- app/helpers/html_validation_helper.rb
+++ app/helpers/html_validation_helper.rb
@@ -2,7 +2,6 @@ require "nokogiri"
 
 module HtmlValidationHelper
   def self.valid_html?(html_string)
-    # binding.pry
     doc = Nokogiri.HTML(html_string) { |config| config.strict }
     return !doc.errors.any?
   rescue StandardError => e
