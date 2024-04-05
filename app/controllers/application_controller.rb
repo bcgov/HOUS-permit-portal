0845a44920cf227231356a41c2f03727a4a488d4
Derek Yau <derek@laterolabs.com>
2024-04-05 09:18:32 -0700

    temporarily add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
+  if Rails.env.production?
+    http_basic_authenticate_with name: "latero-test-prod", password: ENV["TEMP_BASIC_AUTH_PASSWORD"]
+  end
+
   skip_before_action :verify_authenticity_token
 end
