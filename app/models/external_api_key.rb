e728ea1c52d3f342f78193c843e17ef725cf3152
Tawshif Alam <tawshif@laterolabs.com>
2024-04-17 16:41:56 -0700

    [bphh-1190] Add token namespace, and set token to always be auto-generated on create. Also adds spec to ensure token is encrypted

--- app/models/external_api_key.rb
+++ app/models/external_api_key.rb
@@ -1,4 +1,8 @@
 class ExternalApiKey < ApplicationRecord
+  # Token namespace can be useful for secrets scanning tools, e.g. GitHub secret scanning
+  # https://docs.github.com/en/code-security/secret-scanning/secret-scanning-partner-program
+  TOKEN_NAMESPACE = "bphh"
+
   belongs_to :jurisdiction
 
   validates :token, uniqueness: true
@@ -11,6 +15,6 @@ class ExternalApiKey < ApplicationRecord
   private
 
   def generate_token
-    self.token ||= SecureRandom.urlsafe_base64(64)
+    self.token = "#{TOKEN_NAMESPACE}_#{SecureRandom.urlsafe_base64(64)}"
   end
 end
