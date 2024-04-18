f155deff600fb85ca5d1529d8a8eca9f03e41089
Tawshif Alam <tawshif@laterolabs.com>
2024-04-18 13:10:20 -0700

    [bphh-1190] Rename expired_date to expired_at and add revoked_at column to external api key. Adds scope to retrieve active api_keys and adds spec for the scope

--- db/migrate/20240417195959_create_external_api_keys.rb
+++ db/migrate/20240417195959_create_external_api_keys.rb
@@ -2,7 +2,8 @@ class CreateExternalApiKeys < ActiveRecord::Migration[7.1]
   def change
     create_table :external_api_keys, id: :uuid do |t|
       t.string :token, null: false, limit: 510
-      t.datetime :expiration_date, null: true
+      t.datetime :expired_at, null: true
+      t.datetime :revoked_at, null: true
       t.string :name, null: false
       t.string :webhook_url, null: true
       t.references :jurisdiction, null: false, foreign_key: true, type: :uuid
--- spec/factories/external_api_key.rb
+++ spec/factories/external_api_key.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :external_api_key do
     association :jurisdiction, factory: :sub_district
     name { Faker::Lorem.words(number: 2).join(" ") }
-    expiration_date { nil }
+    expired_at { nil }
+    revoked_at { nil }
   end
 end
