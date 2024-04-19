2f3cb6637cdcec5f927f213248f38d22d2af750a
Tawshif Alam <tawshif@laterolabs.com>
2024-04-19 11:58:49 -0700

    [bphh-1190] Add external_api_key controller and routes

---
+++ app/controllers/api/external_api_keys_controller.rb
@@ -0,0 +1,85 @@
+class Api::ExternalApiKeysController < Api::ApplicationController
+  before_action :set_external_api_key, except: %i[index create]
+
+  def index
+    @external_api_key = policy_scope(ExternalApiKey)
+
+    render_success @external_api_key, nil, { blueprint: ExternalApiKeyBlueprint }
+  end
+
+  def show
+    authorize @external_api_key
+
+    render_success @external_api_key, nil, { blueprint: ExternalApiKeyBlueprint, blueprint_opts: { view: :with_token } }
+  end
+
+  def create
+    @external_api_key = ExternalApiKey.build(external_api_key_params)
+
+    authorize @external_api_key
+
+    if @external_api_key.save
+      render_success @external_api_key,
+                     "external_api_key.create_success",
+                     { blueprint: ExternalApiKeyBlueprint, blueprint_opts: { view: :with_token } }
+    else
+      render_error "external_api_key.create_error",
+                   message_opts: {
+                     error_message: @external_api_key.errors.full_messages.join(", "),
+                   }
+    end
+  end
+
+  def update
+    authorize @external_api_key
+
+    if @external_api_key.update(external_api_key_params)
+      render_success @external_api_key,
+                     "external_api_key.update_success",
+                     { blueprint: ExternalApiKeyBlueprint, blueprint_opts: { view: :with_token } }
+    else
+      render_error "external_api_key.update_error",
+                   message_opts: {
+                     error_message: @external_api_key.errors.full_messages.join(", "),
+                   }
+    end
+  end
+
+  def destroy
+    authorize @external_api_key
+
+    if @external_api_key.destroy
+      render json: {}, status: :ok
+    else
+      render_error "external_api_key.delete_error",
+                   message_opts: {
+                     error_message: @external_api_key.errors.full_messages.join(", "),
+                   }
+    end
+  end
+
+  def revoke
+    authorize @external_api_key
+
+    if @external_api_key.revoke
+      render_success @external_api_key,
+                     "external_api_key.revoke_success",
+                     { blueprint: ExternalApiKeyBlueprint, blueprint_opts: { view: :with_token } }
+    else
+      render_error "external_api_key.revoke_error",
+                   message_opts: {
+                     error_message: @external_api_key.errors.full_messages.join(", "),
+                   }
+    end
+  end
+
+  private
+
+  def set_external_api_key
+    @external_api_key = ExternalApiKey.find(params[:id])
+  end
+
+  def external_api_key_params
+    params.require(:external_api_key).permit(:name, :expired_at, :jurisdiction_id, :webhook_url)
+  end
+end
