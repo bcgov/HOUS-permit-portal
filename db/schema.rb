1f1fb0273577a68996baabe1f30dd96eefb169b0
Tawshif Alam <tawshif@laterolabs.com>
2024-04-22 11:09:56 -0700

    [bphh-1208] Add ability to enable or disable external_api_keys for a whole jurisdiction. Update relevant scope, logic, and spec's to handle this

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_17_195959) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_22_165748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -128,6 +128,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_17_195959) do
     t.integer "zero_carbon_step_required", default: 1
     t.string "slug"
     t.integer "map_zoom"
+    t.boolean "external_api_enabled", default: false
     t.index ["prefix"], name: "index_jurisdictions_on_prefix", unique: true
     t.index ["regional_district_id"],
             name: "index_jurisdictions_on_regional_district_id"
--- spec/factories/external_api_key.rb
+++ spec/factories/external_api_key.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :external_api_key do
-    association :jurisdiction, factory: :sub_district
+    association :jurisdiction, factory: :sub_district, external_api_enabled: true
     name { Faker::Lorem.words(number: 2).join(" ") }
     expired_at { nil }
     revoked_at { nil }
