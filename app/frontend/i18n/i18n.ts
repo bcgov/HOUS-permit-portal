f859237abd790b6a867396476e3faa42bc930c3a
laurel <laurel@laterolabs.com>
2024-05-16 13:55:30 -0700

    Allow admins to invite regional review managers
    [BPHH-1438]

--- app/frontend/i18n/i18n.ts
+++ app/frontend/i18n/i18n.ts
@@ -1099,6 +1099,7 @@ const options = {
 
           roles: {
             submitter: "submitter",
+            regional_review_manager: "regional review manager",
             review_manager: "review manager",
             reviewer: "reviewer",
             super_admin: "super admin",
