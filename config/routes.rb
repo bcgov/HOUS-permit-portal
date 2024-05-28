d1b06d9798395a4aa5d6975a3adeb652227ba944
Tawshif Alam <tawshif@laterolabs.com>
2024-05-28 16:13:10 -0700

    [bphh-1288] Api appi doc routes to be consitent in casing

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,8 @@ Rails.application.routes.draw do
   end
 
   mount Sidekiq::Web => "/sidekiq"
-  mount Rswag::Ui::Engine => "/integrations/api-docs"
-  mount Rswag::Api::Engine => "/integrations/api-docs"
+  mount Rswag::Ui::Engine => "/integrations/api_docs"
+  mount Rswag::Api::Engine => "/integrations/api_docs"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", :as => :rails_health_check
