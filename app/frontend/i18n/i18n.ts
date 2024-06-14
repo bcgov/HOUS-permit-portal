d343e214d914ecd1035ccb3302f457079ed160f9
Keith Maibach <keith@laterolabs.ca>
2024-06-14 11:49:51 -0700

    back button copy change

--- app/frontend/i18n/i18n.ts
+++ app/frontend/i18n/i18n.ts
@@ -148,7 +148,7 @@ const options = {
           export: "Export",
           preview: "Preview",
           back: "Back",
-          backHome: "Back to home",
+          backHome: "Back to inbox",
           yes: "Yes",
           no: "No",
           show: "Show",
