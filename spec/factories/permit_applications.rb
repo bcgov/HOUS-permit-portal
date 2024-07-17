6e1acdf2dabbd5e609c699659d155340bfdae7f5
Keith Maibach <keith@laterolabs.ca>
2024-07-17 15:31:12 -0700

    add trait for newly submitted to fix specs

--- spec/factories/permit_applications.rb
+++ spec/factories/permit_applications.rb
@@ -4,9 +4,17 @@ FactoryBot.define do
     association :jurisdiction, factory: :sub_district
     permit_type { PermitType.first || association(:permit_type, code: :low_residential) }
     activity { Activity.first || association(:activity, code: :new_construction) }
-    status { :draft }
+    status { :new_draft }
     sequence(:nickname) { |n| "Permit Application Nickname #{n}" }
     association :template_version
     pid { "999999999" }
+
+    trait :newly_submitted do
+      status { :newly_submitted }
+      after(:create) do |permit_application|
+        create(:submission_version, permit_application: permit_application)
+        permit_application.reindex
+      end
+    end
   end
 end
