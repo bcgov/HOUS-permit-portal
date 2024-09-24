c9920dc1d93f138cdd3b7977bdb5acfb8fe41166
Derek Yau <derek@laterolabs.com>
2024-09-24 13:42:58 -0700

    add request ID to logs so we can find things easier with Kibana

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module HousPermitPortal
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    config.middleware.insert_before 0, ActionDispatch::RequestId, header: "X-Request-Id"
+    config.log_tags = [:request_id]
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
