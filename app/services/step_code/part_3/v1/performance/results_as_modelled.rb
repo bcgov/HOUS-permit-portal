ba7e9e6d8ab528d9081ac8405b21a0761eb06654
laurel <laurel@laterolabs.com>
2024-11-07 15:02:12 -0800

    Add calculation logic for results as modelled
    BPHH-2389

--- app/services/step_code/part_3/v1/performance/results_as_modelled.rb
+++ app/services/step_code/part_3/v1/performance/results_as_modelled.rb
@@ -1,31 +1,63 @@
 class StepCode::Part3::V1::Performance::ResultsAsModelled
+  attr_reader :checklist, :requirements
+
   def initialize(checklist:, requirements:)
+    @checklist = checklist
+    @requirements = requirements
   end
 
   def call
-    # if there are ONLY baseline occupancies, use total energy, otherwise use teui/tedi/ghgi
-
-    # TEUI - total annual energy - total electricity generated onsite / total MFA
-    # TEDI - annual thermal energy demand for TEDI / total MFA
-    # GHGI - total annual emissions / total MFA
     { teui: teui, tedi: tedi, ghgi: ghgi, total_energy: total_energy }
   end
 
   private
 
   def total_energy
-    # total annual anergy - total electricty generated onsite
+    return unless applicable?(:total_energy)
+
+    total_energy_use
   end
 
   def teui
-    # total annual energy - total electricity generated onsite / total MFA
+    return unless applicable?(:teui)
+
+    total_energy_use / total_mfa
   end
 
   def tedi
-    # annual thermal energy demand for TEDI / total MFA
+    return unless applicable?(:tedi)
+
+    checklist.total_annual_thermal_energy_demand / total_mfa
   end
 
   def ghgi
-    # total annual emissions / total MFA
+    return unless applicable?(:ghgi)
+
+    total_emissions / total_mfa
+  end
+
+  def total_emissions
+    @total_emissions ||=
+      checklist
+        .modelled_energy_outputs
+        .joins(:fuel_type)
+        .sum("annual_energy * fuel_types.emissions_factor")
+  end
+
+  def applicable?(metric)
+    compliance_metrics.include?(metric)
+  end
+
+  def compliance_metrics
+    @compliance_metrics ||= checklist.compliance_metrics
+  end
+
+  def total_energy_use
+    @total_energy_use ||=
+      requirements[:total_enetry] - checklist.generated_electricity
+  end
+
+  def total_mfa
+    @total_mfa ||= checklist.occupancies.sum(:modelled_floor_area)
   end
 end
