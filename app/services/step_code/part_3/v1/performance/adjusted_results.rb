d2e8890754bb9de106366df299e138ab86d7323c
laurel <laurel@laterolabs.com>
2024-11-14 20:22:35 -0800

    Add adjusted performance compliance logic
    [BPHH-2392]

--- app/services/step_code/part_3/v1/performance/adjusted_results.rb
+++ app/services/step_code/part_3/v1/performance/adjusted_results.rb
@@ -18,18 +18,45 @@ class StepCode::Part3::V1::Performance::AdjustedResults < StepCode::Part3::V1::P
   private
 
   def total_energy
-    super {}
+    super do
+      return if results_as_modelled[:total_energy] == 0
+
+      (
+        results_as_modelled[:total_energy] -
+          corridor_pressurization_adjustment[:total_energy] +
+          suite_sub_metering_adjustment[:total_energy]
+      ).round(0)
+    end
   end
 
   def teui
-    super {}
+    super do
+      return if results_as_modelled[:teui] == 0
+
+      (
+        results_as_modelled[:teui] - corridor_pressurization_adjustment[:teui] +
+          suite_sub_metering_adjustment[:teui]
+      ).round(0)
+    end
   end
 
   def tedi
-    super {}
+    super do
+      return if results_as_modelled[:tedi] == 0
+
+      (
+        results_as_modelled[:tedi] - corridor_pressurization_adjustment[:tedi]
+      ).round(0)
+    end
   end
 
   def ghgi
-    super {}
+    super do
+      return if results_as_modelled[:ghgi] == 0
+
+      (
+        results_as_modelled[:ghgi] - corridor_pressurization_adjustment[:ghgi]
+      ).round(0)
+    end
   end
 end
