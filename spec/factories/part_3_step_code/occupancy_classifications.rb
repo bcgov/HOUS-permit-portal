d249ff701f07a1669af1bfa2be0af87b15f149be
laurel <laurel@laterolabs.com>
2024-11-19 14:29:57 -0800

    Include achieved steps in performance results
    [BPHH-2435]
    
    Refactor so energy and zero carbon step requirements are no longer
    stored as an enum. This simplifies the code and facilitates the reuse
    of the step proposal logic for part 9 checklists.

--- spec/factories/part_3_step_code/occupancy_classifications.rb
+++ spec/factories/part_3_step_code/occupancy_classifications.rb
@@ -2,15 +2,15 @@ FactoryBot.define do
   factory :step_code_occupancy, class: Part3StepCode::OccupancyClassification do
     occupancy_type { :step_code }
     key { :hotel_motel }
-    energy_step_required { :step_3 }
-    zero_carbon_step_required { :el_1 }
+    energy_step_required { 3 }
+    zero_carbon_step_required { 1 }
     modelled_floor_area { 1000 }
     performance_requirement { nil }
 
     trait :other_residential do
       key { :residential }
-      energy_step_required { :step_3 }
-      zero_carbon_step_required { :el_4 }
+      energy_step_required { 3 }
+      zero_carbon_step_required { 4 }
     end
 
     trait :low_industrial do
