758d780b743ee6924560c6a11ab5a97eb13ab24b
Keith Maibach <keith@laterolabs.ca>
2024-12-03 14:38:04 -0800

    fix formatting

--- app/frontend/components/domains/permit-application/permit-application-filters-menu.tsx
+++ app/frontend/components/domains/permit-application/permit-application-filters-menu.tsx
@@ -1,80 +1,78 @@
-import {
-  Button,
-  Checkbox,
-  CheckboxGroup,
-  Divider,
-  Flex,
-  Menu,
-  MenuButton,
-  MenuList,
-} from "@chakra-ui/react";
-import React, { useState } from "react";
-import { useTranslation } from "react-i18next";
-import { Funnel, CaretDown } from "@phosphor-icons/react"
+import { Button, Checkbox, CheckboxGroup, Divider, Flex, Menu, MenuButton, MenuList } from "@chakra-ui/react"
+import { CaretDown, Funnel } from "@phosphor-icons/react"
 import { observer } from "mobx-react-lite"
-import { ISearch } from "../../../lib/create-search-model";
-import { useMst } from "../../../setup/root";
+import React, { useState } from "react"
+import { useTranslation } from "react-i18next"
+import { useMst } from "../../../setup/root"
 
 interface ISearchSortProps {
-  i18nPrefix: string;
-  sortFields: string[];
+  i18nPrefix: string
+  sortFields: string[]
 }
 
-export const PermitApplicationFiltersMenu: React.FC<ISearchSortProps> = observer(({
-  i18nPrefix,
-  sortFields,
-}: ISearchSortProps) => {
-  const { t } = useTranslation();
-  const {permitApplicationStore} = useMst()
-  const {hasCollaboratorFilter, setHasCollaboratorFilter, searchPermitApplications, resetFilters} = permitApplicationStore
-  const [isOpen, setIsOpen] = useState(false)
+export const PermitApplicationFiltersMenu: React.FC<ISearchSortProps> = observer(
+  ({ i18nPrefix, sortFields }: ISearchSortProps) => {
+    const { t } = useTranslation()
+    const { permitApplicationStore } = useMst()
+    const { hasCollaboratorFilter, setHasCollaboratorFilter, searchPermitApplications, resetFilters } =
+      permitApplicationStore
+    const [isOpen, setIsOpen] = useState(false)
 
-  const handleCheckboxChange = (newHasCollaborator: boolean) => {
-     setHasCollaboratorFilter(newHasCollaborator);
-    searchPermitApplications()
-  };
+    const handleCheckboxChange = (newHasCollaborator: boolean) => {
+      setHasCollaboratorFilter(newHasCollaborator)
+      searchPermitApplications()
+    }
 
-  const handleClear = () => {
-    resetFilters()
-    searchPermitApplications()
-  };
+    const handleClear = () => {
+      resetFilters()
+      searchPermitApplications()
+    }
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+    const toggleMenu = () => {
+      setIsOpen(!isOpen)
+    }
 
-  return (
-    <Flex direction="column" position="relative" zIndex={1}>
-      <Menu isOpen={isOpen}>
-        <MenuButton as={Button} variant="outline" size="md" leftIcon={<Funnel />} rightIcon={<CaretDown />} onClick={toggleMenu}>
-          {t("ui.filter")}
-        </MenuButton>
-        <MenuList
-          bg="greys.white"
-          border="1px solid"
-          borderColor="border.light"
-          borderRadius="sm"
-          boxShadow="md"
-          p={4}
-          zIndex={1}
-        >
-          <CheckboxGroup>
-            <Flex direction="column">
-              {hasCollaboratorFilter?.toString()}
-                <Checkbox 
-                checked={!!hasCollaboratorFilter}
-                onChange={(e) => handleCheckboxChange(!!e.target.checked)}  
-                mb={2}>
+    return (
+      <Flex direction="column" position="relative" zIndex={1}>
+        <Menu isOpen={isOpen}>
+          <MenuButton
+            as={Button}
+            variant="outline"
+            size="md"
+            leftIcon={<Funnel />}
+            rightIcon={<CaretDown />}
+            onClick={toggleMenu}
+          >
+            {t("ui.filter")}
+          </MenuButton>
+          <MenuList
+            bg="greys.white"
+            border="1px solid"
+            borderColor="border.light"
+            borderRadius="sm"
+            boxShadow="md"
+            p={4}
+            zIndex={1}
+          >
+            <CheckboxGroup>
+              <Flex direction="column">
+                {hasCollaboratorFilter?.toString()}
+                <Checkbox
+                  checked={!!hasCollaboratorFilter}
+                  onChange={(e) => handleCheckboxChange(!!e.target.checked)}
+                  mb={2}
+                >
                   {t(`${i18nPrefix}.filterMenu.collaborating`)}
                 </Checkbox>
-            </Flex>
-          </CheckboxGroup>
-          <Divider borderColor="greys.grey03" my={4} />
-          <Button size="sm" variant="tertiary" onClick={handleClear}>
-            {t("ui.reset")}
-          </Button>
-        </MenuList>
-      </Menu>
-    </Flex>
-  );
-});
+              </Flex>
+            </CheckboxGroup>
+            <Divider borderColor="greys.grey03" my={4} />
+            <Button size="sm" variant="tertiary" onClick={handleClear}>
+              {t("ui.reset")}
+            </Button>
+          </MenuList>
+        </Menu>
+      </Flex>
+    )
+  }
+)
