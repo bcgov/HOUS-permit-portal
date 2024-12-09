2fa417c2958aa450618fcd55db0ec1eda344dda4
Tawshif Alam <tawshif@laterolabs.com>
2024-12-09 10:36:12 -0800

    BPHH-2474 Add migration for use_type enum field on energy_outputs. Change name field to be nullable and add presence validation

--- app/models/part_3_step_code/energy_output.rb
+++ app/models/part_3_step_code/energy_output.rb
@@ -4,5 +4,20 @@ class Part3StepCode::EnergyOutput < ApplicationRecord
   belongs_to :checklist
   belongs_to :fuel_type
 
+  # We only require the name if the use type is other as it's user defined
+  validates :name, presence: true, if: -> { use_type == "other" }
+
   enum source: %i[modelled reference]
+  enum use_type: {
+         interior_lighting: 0,
+         exterior_lighting: 1,
+         heating_general: 2,
+         cooling: 3,
+         pumps: 4,
+         fans: 5,
+         domestic_hot_water: 6,
+         plug_loads: 7,
+         heating_gas: 8,
+         other: 9
+       }
 end
