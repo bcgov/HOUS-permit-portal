5f33dafad1779139d5e469b720baf5a5a7ec9ad7
Tawshif Alam <tawshif@laterolabs.com>
2024-12-11 14:53:00 -0800

    BPHH-2474 Fix rebase issues

--- app/blueprints/step_code/part_3/checklist_blueprint.rb
+++ app/blueprints/step_code/part_3/checklist_blueprint.rb
@@ -57,6 +57,7 @@ class StepCode::Part3::ChecklistBlueprint < Blueprinter::Base
     checklist.fuel_types + Part3StepCode::FuelType.defaults
   end
   association :reference_energy_outputs,
+              blueprint: StepCode::Part3::EnergyOutputBlueprint
   association :modelled_energy_outputs,
               blueprint: StepCode::Part3::EnergyOutputBlueprint
 end
