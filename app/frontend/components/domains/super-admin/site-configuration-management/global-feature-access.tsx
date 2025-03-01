1db0171a6735568635dd667a87719cb4417add19
guri-dev <gurcharan.singh@gov.bc.ca>
2025-03-01 12:32:57 -0800

    BHPP-2690 - Updated PR with review changes

--- app/frontend/components/domains/super-admin/site-configuration-management/global-feature-access.tsx
+++ app/frontend/components/domains/super-admin/site-configuration-management/global-feature-access.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Divider, Flex, Heading, HStack, LinkOverlay, Text, VStack } from "@chakra-ui/react"
+import { Box, Container, Divider, Flex, Heading, HStack, LinkOverlay, Table, Tbody, Td, Text, Thead, Tr, VStack } from "@chakra-ui/react"
 import { ArrowRight } from "@phosphor-icons/react"
 import { observer } from "mobx-react-lite"
 import React, { useEffect, useState } from "react"
@@ -11,44 +11,56 @@ export const AdminGlobalFeatureAccessScreen = observer(() => {
   const { t } = useTranslation()
   const { siteConfigurationStore } = useMst()
   const [submissionInboxState, setSubmissionInboxState] = useState(false)
+  const [isHovered, setIsHovered] = useState(false);
   useEffect(() => {
     setSubmissionInboxState(siteConfigurationStore?.inboxEnabled)
   })
 
   return (
-    <Container maxW="container.lg" p={8} as={"main"}>
-      <VStack alignItems={"flex-start"} spacing={5} w={"full"} h={"full"}>
-        <Flex justifyContent={"space-between"} w={"full"} alignItems={"flex-end"}>
-          <Box>
-            <Heading as="h1">{t(`${i18nPrefix}.title`)}</Heading>
-            <Text color="text.secondary" my={6}>
-              {t(`${i18nPrefix}.description`)}
-            </Text>
-          </Box>
-        </Flex>
-      </VStack>
-      <Flex justifyContent={"space-between"} w={"full"} alignItems={"flex-end"}>
-        <VStack justifyContent={"space-between"} w={"full"} alignItems={"flex-end"}>
-          <Flex direction="row" justify="space-between" w="100%">
-            <Text>{t(`${i18nPrefix}.submissionInbox`)}</Text>
-            <HStack spacing={5} align="center">
-              <LinkOverlay
+    <Container maxW="container.lg" p={8} as={"main"} py={8} flexGrow={1}>
+      <Box w="full">
+        <Heading as="h1" pt={3}>{t(`${i18nPrefix}.title`)}</Heading>
+        <Text color="text.secondary" my={6}>
+          {t(`${i18nPrefix}.description`)}
+        </Text>
+      </Box>
+      <Box w="full">
+        <Table variant="simple">
+          <Tbody>
+            <Tr borderTop="none" style={{ borderBottom: "1px solid #D9D9D9" }}>
+              <Td width="1000px" _hover={{ color: '#1A5A96' }} >
+                <Text>{t(`${i18nPrefix}.submissionInbox`)}</Text>
+              </Td>
+              <Td width="80px">
+                <Text fontWeight="bold" textAlign="end"  color="black" textDecoration="none">
+                  {submissionInboxState === false ? t(`${i18nPrefix}.toggleOff`) : t(`${i18nPrefix}.toggleOn`)}
+                </Text>
+              </Td>
+              <Td width="50px" paddingLeft="0">
+                <LinkOverlay
                 as={RouterLinkButton}
                 to="submission-inbox"
                 variant="link"
                 textDecoration="none"
                 position="relative"
-              >
-                <HStack spacing={6}>
-                  <Text fontWeight="normal" color="black" textDecoration="none">
-                    {submissionInboxState === false ? t(`${i18nPrefix}.toggleOff`) : t(`${i18nPrefix}.toggleOn`)}
-                  </Text>
-                  <ArrowRight color="black" size={16} />
-                </HStack>
-              </LinkOverlay>
-            </HStack>
+                onMouseEnter={() => setIsHovered(true)} 
+                onMouseLeave={() => setIsHovered(false)}
+                >
+                  <ArrowRight 
+                    color={isHovered ? '#1A5A96' : 'black'} 
+                    size={20} 
+                  />
+                </LinkOverlay>
+              </Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </Box>
+      <Flex justifyContent={"space-between"} w={"full"} alignItems={"flex-end"}>
+        <VStack justifyContent={"space-between"} w={"full"} alignItems={"flex-end"}>
+          <Flex direction="row" justify="space-between" w="100%">
+          
           </Flex>
-          <Divider />
         </VStack>
       </Flex>
     </Container>
---
+++ app/frontend/components/shared/buttons/switch-button.tsx
@@ -0,0 +1,39 @@
+import { Switch, SwitchProps, Flex } from "@chakra-ui/react";
+import { Check, X } from "@phosphor-icons/react"
+import React from "react";
+
+interface ISwitchButton extends SwitchProps {
+  enable: boolean;
+  update: (checked: boolean) => void;
+  size: string;
+}
+
+export const SwitchButton = ({ enable, update, size, ...rest }: ISwitchButton) => {
+  return (
+    <Switch
+      id="inbox-enabled-switch"
+      isChecked={enable}
+      onChange={(e) => update(e.target.checked)}
+      size={size}
+      colorScheme="blue"
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      position="relative"
+      {...rest}
+    >
+      <Flex
+        position="absolute"
+        top="34%"
+        left={enable ? "54%" : "15%"}
+        fontSize="20px"
+      >
+        {enable ? (
+          <Check size={10} color={"black"} />
+        ) : (
+          <X size={10} color={"black"} />
+        )}
+      </Flex>
+    </Switch>
+  );
+};
