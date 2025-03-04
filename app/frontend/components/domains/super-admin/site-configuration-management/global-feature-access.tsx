dae45d0dbb2bfa82819fbe08a1f58744f3f7af5b
guri-dev <gurcharan.singh@gov.bc.ca>
2025-03-04 15:18:40 -0800

    Updated hover effect on the row instead of each column

--- app/frontend/components/domains/super-admin/site-configuration-management/global-feature-access.tsx
+++ app/frontend/components/domains/super-admin/site-configuration-management/global-feature-access.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Divider, Flex, Heading, HStack, LinkOverlay, Table, Tbody, Td, Text, Thead, Tr, VStack } from "@chakra-ui/react"
+import { Box, Container, Flex, Heading, LinkOverlay, Table, Tbody, Td, Text, Tr, VStack } from "@chakra-ui/react"
 import { ArrowRight } from "@phosphor-icons/react"
 import { observer } from "mobx-react-lite"
 import React, { useEffect, useState } from "react"
@@ -11,7 +11,7 @@ export const AdminGlobalFeatureAccessScreen = observer(() => {
   const { t } = useTranslation()
   const { siteConfigurationStore } = useMst()
   const [submissionInboxState, setSubmissionInboxState] = useState(false)
-  const [isHovered, setIsHovered] = useState(false);
+  const [isHovered, setIsHovered] = useState(false)
   useEffect(() => {
     setSubmissionInboxState(siteConfigurationStore?.inboxEnabled)
   })
@@ -19,7 +19,9 @@ export const AdminGlobalFeatureAccessScreen = observer(() => {
   return (
     <Container maxW="container.lg" p={8} as={"main"} py={8} flexGrow={1}>
       <Box w="full">
-        <Heading as="h1" pt={3}>{t(`${i18nPrefix}.title`)}</Heading>
+        <Heading as="h1" pt={3}>
+          {t(`${i18nPrefix}.title`)}
+        </Heading>
         <Text color="text.secondary" my={6}>
           {t(`${i18nPrefix}.description`)}
         </Text>
@@ -27,29 +29,30 @@ export const AdminGlobalFeatureAccessScreen = observer(() => {
       <Box w="full">
         <Table variant="simple">
           <Tbody>
-            <Tr borderTop="none" style={{ borderBottom: "1px solid #D9D9D9" }}>
-              <Td width="1000px" _hover={{ color: '#1A5A96' }} >
+            <Tr
+              borderTop="none"
+              style={{ borderBottom: "1px solid #D9D9D9" }}
+              _hover={{ color: "#1A5A96" }}
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              <Td width="1000px">
                 <Text>{t(`${i18nPrefix}.submissionInbox`)}</Text>
               </Td>
               <Td width="80px">
-                <Text fontWeight="bold" textAlign="end"  color="black" textDecoration="none">
+                <Text fontWeight="bold" textAlign="end" color="black" textDecoration="none">
                   {submissionInboxState === false ? t(`${i18nPrefix}.toggleOff`) : t(`${i18nPrefix}.toggleOn`)}
                 </Text>
               </Td>
               <Td width="50px" paddingLeft="0">
                 <LinkOverlay
-                as={RouterLinkButton}
-                to="submission-inbox"
-                variant="link"
-                textDecoration="none"
-                position="relative"
-                onMouseEnter={() => setIsHovered(true)} 
-                onMouseLeave={() => setIsHovered(false)}
+                  as={RouterLinkButton}
+                  to="submission-inbox"
+                  variant="link"
+                  textDecoration="none"
+                  position="relative"
                 >
-                  <ArrowRight 
-                    color={isHovered ? '#1A5A96' : 'black'} 
-                    size={20} 
-                  />
+                  <ArrowRight color={isHovered ? "#1A5A96" : "black"} size={20} />
                 </LinkOverlay>
               </Td>
             </Tr>
@@ -58,9 +61,7 @@ export const AdminGlobalFeatureAccessScreen = observer(() => {
       </Box>
       <Flex justifyContent={"space-between"} w={"full"} alignItems={"flex-end"}>
         <VStack justifyContent={"space-between"} w={"full"} alignItems={"flex-end"}>
-          <Flex direction="row" justify="space-between" w="100%">
-          
-          </Flex>
+          <Flex direction="row" justify="space-between" w="100%"></Flex>
         </VStack>
       </Flex>
     </Container>
