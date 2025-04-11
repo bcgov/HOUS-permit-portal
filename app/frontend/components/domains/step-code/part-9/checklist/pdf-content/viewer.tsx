3a4bf1f14da8fa9bddb268339e8a03687d3b5798
Keith M <keith@laterolabs.com>
2025-04-11 15:57:25 -0700

    initial code for reusing part 9 pdf viewer for part 3

--- app/frontend/components/domains/step-code/part-9/checklist/pdf-content/viewer.tsx
+++ app/frontend/components/domains/step-code/part-9/checklist/pdf-content/viewer.tsx
@@ -3,19 +3,23 @@ import { observer } from "mobx-react-lite"
 import React, { Suspense, useEffect } from "react"
 import { PDFContent } from "."
 import { usePermitApplication } from "../../../../../../hooks/resources/use-permit-application"
-import { IStepCodeChecklist } from "../../../../../../models/part-9-step-code-checklist"
+import { IPart3StepCodeChecklist } from "../../../../../../models/part-3-step-code-checklist"
+import { IPart9StepCodeChecklist } from "../../../../../../models/part-9-step-code-checklist"
 import { IPermitApplication } from "../../../../../../models/permit-application"
 import { LoadingScreen } from "../../../../../shared/base/loading-screen"
 
+// Union type for checklists
+type TChecklist = IPart9StepCodeChecklist | IPart3StepCodeChecklist
+
 interface IProps {
   mode: "html" | "pdf"
+  checklist: TChecklist
 }
 
-export const StepCodeChecklistPDFViewer = observer(function StepCodeChecklistPDFViewer({ mode }: IProps) {
+export const StepCodeChecklistPDFViewer = observer(function StepCodeChecklistPDFViewer({ mode, checklist }: IProps) {
   // permitApplication must be passed as props to <Content> due to a limitation of react-pdf
   // see: https://github.com/diegomura/react-pdf/issues/2263#issuecomment-1511800981
   const { currentPermitApplication } = usePermitApplication()
-  const checklist = currentPermitApplication?.stepCode?.preConstructionChecklist
   useEffect(() => {
     const fetch = async () => await checklist.load()
     checklist && !checklist.isLoaded && fetch()
@@ -32,13 +36,23 @@ export const StepCodeChecklistPDFViewer = observer(function StepCodeChecklistPDF
 
 interface IContentProps {
   permitApplication: IPermitApplication
-  checklist: IStepCodeChecklist
+  checklist: TChecklist
 }
 
 function Content({ permitApplication, checklist }: IContentProps) {
+  // Check for a property unique to Part 9 checklist ('stage')
+  const isPart9 = "stage" in checklist
+
   return (
     <Suspense fallback={<LoadingScreen />}>
-      {checklist?.isLoaded && <PDFContent permitApplication={permitApplication} checklist={checklist} />}
+      {checklist?.isLoaded && isPart9 && (
+        <PDFContent permitApplication={permitApplication} checklist={checklist as IPart9StepCodeChecklist} />
+      )}
+      {checklist?.isLoaded && !isPart9 && (
+        // Replace with actual Part 3 component later
+        // We'll need to import IPart3StepCodeChecklist here when we add the real component
+        <div>Part 3 PDF Content for {checklist.id} Placeholder</div>
+      )}
     </Suspense>
   )
 }
