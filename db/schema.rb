f488c723fdf8fdc73932e8b73c76f0b28b7b0b08
Keith M <keith@laterolabs.com>
2025-04-15 15:00:09 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_13_062923) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_18_213843) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -582,6 +582,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_13_062923) do
             null: false
     t.jsonb "revision_reason_options"
     t.uuid "small_scale_requirement_template_id"
+    t.boolean "inbox_enabled", default: false, null: false
     t.index ["small_scale_requirement_template_id"],
             name: "idx_on_small_scale_requirement_template_id_235b636c86"
   end
