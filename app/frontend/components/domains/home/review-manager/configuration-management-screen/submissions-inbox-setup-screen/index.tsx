1704d927d0c5f7e0b5d548ba2d91e0b7948c4266
guri-dev <gurcharan.singh@gov.bc.ca>
2025-05-27 18:40:27 -0700

    Merge feature access into develop branch and removed the old submission inbox set up from configuration

--- app/frontend/components/domains/home/review-manager/configuration-management-screen/submissions-inbox-setup-screen/index.tsx
+++ app/frontend/components/domains/home/review-manager/configuration-management-screen/submissions-inbox-setup-screen/index.tsx
@@ -1,5 +1,4 @@
-import { Container, Heading, Text, VStack } from "@chakra-ui/react"
-import { t } from "i18next"
+import { Container, VStack } from "@chakra-ui/react"
 import { observer } from "mobx-react-lite"
 import React, { Suspense } from "react"
 import { useJurisdiction } from "../../../../../../hooks/resources/use-jurisdiction"
@@ -7,7 +6,6 @@ import { usePermitClassificationsLoad } from "../../../../../../hooks/resources/
 import { ErrorScreen } from "../../../../../shared/base/error-screen"
 import { LoadingScreen } from "../../../../../shared/base/loading-screen"
 import { Form } from "./form"
-import { i18nPrefix } from "./i18n-prefix"
 
 export const SubmissionsInboxSetupScreen = observer(function SubmissionsInboxSetupScreen() {
   const { isLoaded } = usePermitClassificationsLoad()
@@ -19,14 +17,6 @@ export const SubmissionsInboxSetupScreen = observer(function SubmissionsInboxSet
   ) : (
     <Container maxW="container.lg" py={8} px={{ base: 8, xl: 0 }}>
       <VStack spacing={8} align="start" w="full">
-        <VStack spacing={0.5} align="start" w="full">
-          <Heading mb={0} fontSize="3xl">
-            {t(`${i18nPrefix}.title`)}
-          </Heading>
-          <Text fontSize="sm" color="text.secondary">
-            {t(`${i18nPrefix}.description`)}
-          </Text>
-        </VStack>
         <Suspense fallback={<LoadingScreen />}>
           {isLoaded && currentJurisdiction && <Form jurisdiction={currentJurisdiction} />}
         </Suspense>
