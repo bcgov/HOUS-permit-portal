c18cbaecb9301cc801459084098e5c82eaf7ad4a
Keith M <keith@laterolabs.com>
2025-06-10 09:22:47 -0700

    optional logging of pdf jsons

--- app/jobs/pdf_generation_job.rb
+++ app/jobs/pdf_generation_job.rb
@@ -159,6 +159,19 @@ class PdfGenerationJob
         else
           err = "Pdf generation process failed: #{exit_status}"
           Rails.logger.error err
+          # Log the failing JSON data for debugging if a release version is set
+          # This environment variable should only be present on test and dev environments
+          if ENV["LOG_POTENTIALLY_SENSITIVE"] == "true"
+            begin
+              parsed_json = JSON.parse(pdf_json_data)
+              Rails.logger.error "###############################"
+              Rails.logger.error "Failing PDF JSON data:\n#{JSON.pretty_generate(parsed_json)}"
+              Rails.logger.error "###############################"
+            rescue JSON::ParserError
+              Rails.logger.error "Failing PDF JSON data (could not be parsed):\n#{pdf_json_data}"
+              Rails.logger.error "###############################"
+            end
+          end
 
           # this will raise an error and retry the job
           raise err
