43264cc7e756ed3652d6a7f71a1c180cde09ca61
guri-dev <gurcharan.singh@gov.bc.ca>
2025-06-19 11:46:42 -0700

    HUB-1704 copy updates for super admin configuration block

--- app/frontend/i18n/i18n.ts
+++ app/frontend/i18n/i18n.ts
@@ -2284,11 +2284,10 @@ const options = {
             },
           },
           configurationManagement: {
-            title: "Configuration management",
+            title: "Configuration",
             reviewManagerDescription:
               "Configure your jurisdiction's operational setup within the Building Permit Hub. Set up your submission inbox, define energy step codes, and edit the 'About' page to reflect specific local information.",
-            adminDescription:
-              "Manage site-wide settings and messages along with providing administrative control to Super Admins.",
+            adminDescription: "Manage system-wide settings, notifications, and administrative access",
             jurisdictionLocalityTypeLabel: "Locality type of local jurisdiction",
             jurisdictionNameLabel: "Name of local jurisdiction",
             jurisdictionLocationLabel: "Location",
