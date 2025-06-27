f9be482105c9353534103f10c2c7e51c49268f23
guri-dev <gurcharan.singh@gov.bc.ca>
2025-06-27 13:04:44 -0700

    HUB-3152 updated logic button text for Super Admin and for other users

--- app/frontend/components/domains/landing/index.tsx
+++ app/frontend/components/domains/landing/index.tsx
@@ -93,7 +93,7 @@ export const LandingScreen = observer(({}: ILandingScreenProps) => {
               >
                 {currentUser
                   ? t("landing.goTo", {
-                      location: currentUser?.isSubmitter ? t("landing.submitterPanel") : t("landing.adminPanel"),
+                      location: currentUser?.isSuperAdmin ? t("landing.adminPanel") : t("landing.projectsPanel"),
                     })
                   : t("landing.permitApp")}
               </RouterLinkButton>
