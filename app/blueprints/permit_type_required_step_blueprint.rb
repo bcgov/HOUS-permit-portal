d49e2644b6ca0830ab63e03d292c21d1140c4f38
Keith M <keith@laterolabs.com>
2025-07-17 09:24:35 -0700

    revert activity name logic to previous wip solution

--- app/blueprints/permit_type_required_step_blueprint.rb
+++ app/blueprints/permit_type_required_step_blueprint.rb
@@ -4,7 +4,11 @@ class PermitTypeRequiredStepBlueprint < Blueprinter::Base
   fields :permit_type_id,
          :energy_step_required,
          :zero_carbon_step_required,
-         :default,
-         :active_name,
-         :permit_type_name
+         :permit_type_name,
+         :default
+
+  field :activity_name do |ptr_step, _options|
+    template = RequirementTemplate.find_by(permit_type: ptr_step.permit_type)
+    template&.activity&.name
+  end
 end
