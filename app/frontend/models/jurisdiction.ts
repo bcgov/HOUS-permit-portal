85944e7f5db77e5dd1db736c775b8cbf6f8f65af
Keith M <keith@laterolabs.com>
2025-07-25 17:11:16 -0700

    update step code requiremnet accordions to remove non part 9

--- app/frontend/models/jurisdiction.ts
+++ app/frontend/models/jurisdiction.ts
@@ -97,17 +97,13 @@ export const JurisdictionModel = types
     },
   }))
   .views((self) => ({
-    get requiredStepsByPermitType() {
-      const groupRequirements = (acc, r) =>
-        R.includes(r, self.permitTypeStepRequirements(r.permitTypeId)) ? acc.concat(r) : acc
-      const toPermitType = ({ permitTypeId }) => permitTypeId
-
-      return R.reduceBy(groupRequirements, [], toPermitType, self.permitTypeRequiredSteps)
-    },
     get part9RequiredSteps() {
-      return self.permitTypeRequiredSteps.filter(
-        (r) => r.permitTypeId == self.rootStore.permitClassificationStore.part9BuildingPermitType.id
-      )
+      // TODO: fix this once we have a proper permit type code reference
+      if (self.permitTypeRequiredSteps.length === 0) return []
+      // return self.permitTypeRequiredSteps.filter((r) => r.permitTypeName.includes("Part 9"))
+      // The above is not necessary as long as data migration is run
+      // Once we add new required steps for other permit types, we can add a reference by code and fitler here, depending on the chosen architecture
+      return self.permitTypeRequiredSteps
     },
   }))
   .actions((self) => ({
