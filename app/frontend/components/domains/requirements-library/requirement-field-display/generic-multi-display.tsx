496832759f80e75f95259880223525f1fa007129
Keith M <keith@laterolabs.com>
2025-11-12 14:00:22 -0800

    fix preview drawer layout and datagrid ux

--- app/frontend/components/domains/requirements-library/requirement-field-display/generic-multi-display.tsx
+++ app/frontend/components/domains/requirements-library/requirement-field-display/generic-multi-display.tsx
@@ -43,7 +43,7 @@ export function GenericMultiDisplay<TFieldValues>({
   return (
     <Box>
       <Box
-        w={"full"}
+        w={"536px"}
         as={"section"}
         borderRadius={"sm"}
         border={"1px solid"}
