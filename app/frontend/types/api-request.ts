9800585b31b97d33cbb19673283b8bd3b4ab0097
Keith M <keith@laterolabs.com>
2025-11-26 14:44:52 -0800

    use site select for project editing

--- app/frontend/types/api-request.ts
+++ app/frontend/types/api-request.ts
@@ -149,6 +149,8 @@ export interface IPermitProjectUpdateParams {
   description?: string
   title?: string
   fullAddress?: string
+  pid?: string
+  jurisdictionId?: string
   // other PermitProject fields that can be updated
   projectDocumentsAttributes?: IProjectDocumentAttribute[]
 }
