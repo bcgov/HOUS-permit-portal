8ec6fed2831a21d26965c4404c0a9a60a3d607af
Keith M <keith@laterolabs.com>
2025-12-17 15:43:56 -0800

    fix specs

--- spec/factories/requirement_blocks.rb
+++ spec/factories/requirement_blocks.rb
@@ -28,7 +28,7 @@ FactoryBot.define do
             "requirement_code" => "energy_step_code_method",
             "label" =>
               "Which method do you want to do use for the energy step code:",
-            "input_type" => "select",
+            "input_type" => "radio",
             "input_options" => {
               "value_options" => [
                 {
