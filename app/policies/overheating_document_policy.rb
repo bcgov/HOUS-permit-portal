e53ce227e079d24b3d3bc362c8ea8fa865702525
guri-dev <gurcharan.singh@gov.bc.ca>
2025-12-19 13:54:47 -0800

    HUB-3628 PR review chnages, better handling of filedownload and upload

--- app/policies/overheating_document_policy.rb
+++ app/policies/overheating_document_policy.rb
@@ -1,5 +1,5 @@
 class OverheatingDocumentPolicy < ApplicationPolicy
   def download?
-    user.present? && (record.pdf_form.user_id == user.id || user.review_staff?)
+    user.present? && record.pdf_form.user_id == user.id
   end
 end
