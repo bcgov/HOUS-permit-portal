b32fb705481f9050ea45162147c900139c7e557e
Keith M <keith@laterolabs.com>
2026-02-05 14:20:38 -0800

    consolidate overheating migrations

--- db/migrate/20251224140000_add_pdf_generation_status_to_pdf_forms.rb
+++
@@ -1,9 +0,0 @@
-class AddPdfGenerationStatusToPdfForms < ActiveRecord::Migration[7.2]
-  def change
-    add_column :pdf_forms,
-               :pdf_generation_status,
-               :integer,
-               default: 0,
-               null: false
-  end
-end
--- db/migrate/20260106160000_remove_status_from_pdf_forms.rb
+++
@@ -1,7 +0,0 @@
-class RemoveStatusFromPdfForms < ActiveRecord::Migration[7.2]
-  def change
-    if column_exists?(:pdf_forms, :status)
-      remove_column :pdf_forms, :status, :boolean
-    end
-  end
-end
