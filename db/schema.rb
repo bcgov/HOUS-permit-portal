316a249d2a7604ce4b12a937d93b00da8a22b6e2
Keith M <keith@laterolabs.com>
2026-02-05 14:56:18 -0800

    regenerate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_03_163643) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_05_221726) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -296,11 +296,11 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_03_163643) do
     t.uuid "user_id", null: false
     t.jsonb "form_json", default: {}
     t.string "form_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.jsonb "pdf_file_data"
     t.integer "pdf_generation_status", default: 0, null: false
     t.datetime "discarded_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["discarded_at"], name: "index_overheating_tools_on_discarded_at"
     t.index ["user_id"], name: "index_overheating_tools_on_user_id"
   end
