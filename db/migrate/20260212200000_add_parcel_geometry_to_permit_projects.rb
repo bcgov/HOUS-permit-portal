f4b15fcb861e78f1416b21bcd22742cf55c3162f
guri-dev <gurcharan.singh@gov.bc.ca>
2026-02-19 10:29:45 -0800

    Added condition to check if column exist

--- db/migrate/20260212200000_add_parcel_geometry_to_permit_projects.rb
+++ db/migrate/20260212200000_add_parcel_geometry_to_permit_projects.rb
@@ -1,5 +1,7 @@
 class AddParcelGeometryToPermitProjects < ActiveRecord::Migration[7.2]
   def change
-    add_column :permit_projects, :parcel_geometry, :jsonb, default: nil
+    unless column_exists?(:permit_projects, :parcel_geometry)
+      add_column :permit_projects, :parcel_geometry, :jsonb, default: nil
+    end
   end
 end
