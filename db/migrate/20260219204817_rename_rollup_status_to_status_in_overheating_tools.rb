b81672d4ac8e6c35141cb24bd48e91361a526084
guri-dev <gurcharan.singh@gov.bc.ca>
2026-02-19 16:31:51 -0800

    Removed the rename rollup status migration as updated the status in the initial migration

--- db/migrate/20260219204817_rename_rollup_status_to_status_in_overheating_tools.rb
+++
@@ -1,9 +0,0 @@
-class RenameRollupStatusToStatusInOverheatingTools < ActiveRecord::Migration[
-  7.2
-]
-  def change
-    unless column_exists?(:overheating_tools, :rollup_status)
-      rename_column :overheating_tools, :rollup_status, :status
-    end
-  end
-end
