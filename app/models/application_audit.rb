ad3a819ddaa0398d03b1b8febf124a8b91ce4611
Ian Fonberg <Ian.Fonberg@gov.bc.ca>
2026-02-25 10:44:54 -0800

    HUB-4202 add application audit model with search capabilities

---
+++ app/models/application_audit.rb
@@ -0,0 +1,68 @@
+class ApplicationAudit < Audited::Audit
+  searchkick searchable: %i[
+               omniauth_username
+               permit_application_nickname
+               jurisdiction_name
+             ],
+             word_start: %i[omniauth_username jurisdiction_name],
+             word_middle: %i[permit_application_nickname],
+             text_start: %i[jurisdiction_name]
+
+  def search_data
+    {
+      auditable_type: auditable_type,
+      auditable_id: auditable_id,
+      associated_type: associated_type,
+      associated_id: associated_id,
+      user_id: user_id,
+      action: action,
+      comment: comment,
+      created_at: created_at,
+      omniauth_username: omniauth_username_for_search,
+      permit_application_nickname: permit_application_nickname_for_search,
+      jurisdiction_name: jurisdiction_name_for_search
+    }
+  end
+
+  private
+
+  def omniauth_username_for_search
+    if user && user.respond_to?(:omniauth_username)
+      user.omniauth_username
+    else
+      username
+    end
+  end
+
+  def permit_application_for_search
+    return auditable if auditable_type == "PermitApplication"
+    return associated if associated_type == "PermitApplication"
+    if auditable.respond_to?(:permit_application)
+      return auditable.permit_application
+    end
+
+    nil
+  end
+
+  def permit_application_nickname_for_search
+    pa = permit_application_for_search
+    return nil unless pa
+
+    pa.nickname
+  end
+
+  def jurisdiction_name_for_search
+    pa = permit_application_for_search
+    return pa.jurisdiction_name if pa.respond_to?(:jurisdiction_name)
+
+    if auditable_type == "PermitProject" && auditable.respond_to?(:jurisdiction)
+      return auditable.jurisdiction&.qualified_name
+    end
+    if associated_type == "PermitProject" &&
+         associated.respond_to?(:jurisdiction)
+      return associated.jurisdiction&.qualified_name
+    end
+
+    nil
+  end
+end
