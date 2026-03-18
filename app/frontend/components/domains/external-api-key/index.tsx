2aa237e6d061a6ba663c807354af752e25ab0c2c
Keith M <keith@laterolabs.com>
2026-03-18 11:47:00 -0700

    remove functioning sandbox tag feature

--- app/frontend/components/domains/external-api-key/index.tsx
+++ app/frontend/components/domains/external-api-key/index.tsx
@@ -16,7 +16,6 @@ import { SharedSpinner } from "../../shared/base/shared-spinner"
 import { SearchGrid } from "../../shared/grid/search-grid"
 import { SearchGridItem } from "../../shared/grid/search-grid-item"
 import { RouterLinkButton } from "../../shared/navigation/router-link-button"
-import { ExternalApiKeySandboxTag } from "./external-api-key-sandbox-tag"
 import { ExternalApiKeyStatusTag } from "./external-api-key-status-tag"
 import { GridHeaders } from "./grid-header"
 
@@ -115,11 +114,9 @@ export const ExternalApiKeysIndexScreen = observer(function ExternalApiKeysIndex
                     <ExternalApiKeyStatusTag status={externalApiKey.status} />
                   </SearchGridItem>
                   <SearchGridItem>
-                    {externalApiKey.statusScope ? (
-                      <ExternalApiKeySandboxTag statusScope={externalApiKey.statusScope} />
-                    ) : (
-                      t("sandbox.live")
-                    )}
+                    {externalApiKey.statusScope
+                      ? t(`sandbox.scopeLabels.${externalApiKey.statusScope}`)
+                      : t("sandbox.live")}
                   </SearchGridItem>
                   <SearchGridItem>{format(externalApiKey.createdAt, datefnsTableDateFormat)}</SearchGridItem>
                   <SearchGridItem>
