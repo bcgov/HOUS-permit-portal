44868054dd24578a2def957cb65175ec21893b35
Keith M <keith@laterolabs.com>
2026-03-29 17:19:37 -0700

    ignore jwt zeitwork autoload load

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,8 @@ module HousPermitPortal
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+
+    config.autoload_lib(ignore: %w[assets tasks devise])
     config.autoload_paths << Rails.root.join("app/presenters")
 
     # Configuration for the application, engines, and railties goes here.
