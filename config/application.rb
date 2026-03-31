5ccf1925460d3bb24e7f237fdbfb326650994e9f
Keith M <keith@laterolabs.com>
2026-03-31 14:00:26 -0700

    remove unecessary eager loads

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,6 @@ module HousPermitPortal
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-    config.autoload_paths << Rails.root.join("app/presenters")
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -35,7 +34,6 @@ module HousPermitPortal
     # in config/environments, which are processed later.
     #
     config.time_zone = "Pacific Time (US & Canada)"
-    config.eager_load_paths << Rails.root.join("app/presenters")
 
     # Don't generate system test files.
     config.generators.system_tests = nil
