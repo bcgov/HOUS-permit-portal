5fabb75816fc29a8014ea4a71063d3e82de14768
Ian Fonberg <Ian.Fonberg@gov.bc.ca>
2026-04-14 16:42:13 -0700

    HUB-4524 replace browser history entry instead of pushing

--- app/frontend/components/domains/external-api-key/index.tsx
+++ app/frontend/components/domains/external-api-key/index.tsx
@@ -78,6 +78,7 @@ export const ExternalApiKeysIndexScreen = observer(function ExternalApiKeysIndex
               to={"create"}
               isDisabled={!externalApiEnabled}
               onClick={disableLinkClick}
+              replace
             >
               {t("externalApiKey.index.createExternalApiKey")}
             </RouterLinkButton>
