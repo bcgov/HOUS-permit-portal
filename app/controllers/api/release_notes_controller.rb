8efe6c7beaf93a29a5f2056466907d09b9fbe4ab
Ian Fonberg <Ian.Fonberg@gov.bc.ca>
2026-04-23 14:17:33 -0700

    HUB-4210 add update action for release notes

--- app/controllers/api/release_notes_controller.rb
+++ app/controllers/api/release_notes_controller.rb
@@ -1,4 +1,6 @@
 class Api::ReleaseNotesController < Api::ApplicationController
+  before_action :set_release_note, only: %i[update]
+
   def create
     @release_note = ReleaseNote.new(release_note_params)
     authorize @release_note
@@ -14,11 +16,42 @@ class Api::ReleaseNotesController < Api::ApplicationController
                      }
     else
       render_error "release_note.create_error",
-                   @release_note.errors.full_messages
+                   {
+                     message_opts: {
+                       error_message:
+                         @release_note.errors.full_messages.join(", ")
+                     }
+                   }
+    end
+  end
+
+  def update
+    authorize @release_note
+    if @release_note.update(release_note_params)
+      render_success @release_note,
+                     "release_note.update_success",
+                     {
+                       blueprint: ReleaseNoteBlueprint,
+                       blueprint_opts: {
+                         view: :extended
+                       }
+                     }
+    else
+      render_error "release_note.update_error",
+                   {
+                     message_opts: {
+                       error_message:
+                         @release_note.errors.full_messages.join(", ")
+                     }
+                   }
     end
   end
 
   private
+
+  def set_release_note
+    @release_note = ReleaseNote.find(params[:id])
+  end
 
   def release_note_params
     params.require(:release_note).permit(
