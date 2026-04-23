7f2c221251e90cbed4a75ef11a7b27d7f7015623
Ian Fonberg <Ian.Fonberg@gov.bc.ca>
2026-04-23 14:18:48 -0700

    HUB-4210 update request spec

---
+++ spec/requests/release_notes_spec.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+RSpec.describe "ReleaseNotes", type: :request do
+  include Devise::Test::IntegrationHelpers
+  subject { json_response.fetch("data") }
+  let(:user) { create(:user, :super_admin) }
+  let(:params) do
+    {
+      version: Faker::App.semantic_version,
+      release_date: Faker::Date.between(from: 1.year.ago, to: Time.current),
+      content: Faker::Lorem.paragraph,
+      release_notes_url: Faker::Internet.url,
+      issues: Faker::Lorem.paragraph
+    }
+  end
+  describe "#create" do
+    it "creates a release note" do
+      sign_in user
+      post release_notes_path, params: { release_note: params }
+
+      expect(response).to have_http_status(:success)
+      expect(subject).to include(
+        "version" => params[:version],
+        "content" => params[:content],
+        "release_notes_url" => params[:release_notes_url],
+        "issues" => params[:issues],
+        "updated_at" => be_present
+      )
+      expect(Time.zone.at(subject["release_date"] / 1000).to_date).to eq(
+        params[:release_date]
+      )
+    end
+  end
+
+  describe "#update" do
+    it "updates an existing release note" do
+      sign_in user
+      release_note = create(:release_note)
+      patch release_note_path(release_note.id),
+            params: {
+              release_note: {
+                version: params[:version]
+              }
+            }
+
+      expect(response).to have_http_status(:success)
+      expect(subject).to include("version" => params[:version])
+    end
+  end
+end
