6eb37ce95eb23f724dfc318f84941db1121e1795
Keith M <keith@laterolabs.com>
2026-05-01 15:46:06 -0700

    redact project title from jurisdiction staff and use template instead of pa nickname

--- app/frontend/models/permit-project.ts
+++ app/frontend/models/permit-project.ts
@@ -13,7 +13,7 @@ import { PermitProjectInboxApplicationSearchSlice } from "./permit-project-inbox
 
 const PermitProjectCoreModel = types.model("PermitProjectCore", {
   id: types.identifier,
-  title: types.string,
+  title: types.maybe(types.string),
   fullAddress: types.maybeNull(types.string),
   pid: types.maybeNull(types.string),
   number: types.maybeNull(types.string),
