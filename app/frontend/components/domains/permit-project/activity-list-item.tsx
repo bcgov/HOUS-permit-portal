55b8cdfb19ef2c50dd2c138017b480c03abed39b
Keith M <keith@laterolabs.com>
2026-05-05 16:02:23 -0700

    alternative fix for changing href on activity item

--- app/frontend/components/domains/permit-project/activity-list-item.tsx
+++ app/frontend/components/domains/permit-project/activity-list-item.tsx
@@ -4,21 +4,18 @@ import { observer } from "mobx-react-lite"
 import React from "react"
 import { datefnsTableDateTimeFormat } from "../../../constants"
 import { IProjectAudit } from "../../../models/project-audit"
-import { useMst } from "../../../setup/root"
 import { RouterLink } from "../../shared/navigation/router-link"
 
 interface IActivityListItemProps {
   projectAudit: IProjectAudit
+  fromInbox?: boolean
 }
 
-export const ActivityListItem = observer(({ projectAudit }: IActivityListItemProps) => {
-  const { userStore } = useMst()
+export const ActivityListItem = observer(({ projectAudit, fromInbox = false }: IActivityListItemProps) => {
   const { createdAt, description, permitName, permitApplicationId } = projectAudit
   const permitApplicationPath =
     permitApplicationId &&
-    (userStore.currentUser?.isReviewStaff
-      ? `/permit-applications/${permitApplicationId}`
-      : `/permit-applications/${permitApplicationId}/edit`)
+    (fromInbox ? `/permit-applications/${permitApplicationId}` : `/permit-applications/${permitApplicationId}/edit`)
 
   return (
     <Flex align="center" justify="space-between" py={3} px={4} gap={4} minH={20}>
