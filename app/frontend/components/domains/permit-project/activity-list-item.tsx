6f508f151edf059c5b1025568f4ed74f8fdea210
Ian Fonberg <Ian.Fonberg@gov.bc.ca>
2026-05-05 15:39:26 -0700

    HUB-5010 provide appropriate url for review staff

--- app/frontend/components/domains/permit-project/activity-list-item.tsx
+++ app/frontend/components/domains/permit-project/activity-list-item.tsx
@@ -4,6 +4,7 @@ import { observer } from "mobx-react-lite"
 import React from "react"
 import { datefnsTableDateTimeFormat } from "../../../constants"
 import { IProjectAudit } from "../../../models/project-audit"
+import { useMst } from "../../../setup/root"
 import { RouterLink } from "../../shared/navigation/router-link"
 
 interface IActivityListItemProps {
@@ -11,17 +12,19 @@ interface IActivityListItemProps {
 }
 
 export const ActivityListItem = observer(({ projectAudit }: IActivityListItemProps) => {
+  const { userStore } = useMst()
   const { createdAt, description, permitName, permitApplicationId } = projectAudit
+  const permitApplicationPath =
+    permitApplicationId &&
+    (userStore.currentUser?.isReviewStaff
+      ? `/permit-applications/${permitApplicationId}`
+      : `/permit-applications/${permitApplicationId}/edit`)
 
   return (
     <Flex align="center" justify="space-between" py={3} px={4} gap={4} minH={20}>
       <HStack align="center" spacing={1} flex={1} minW={0}>
         <Text>{description}</Text>
-        {permitName && (
-          <RouterLink to={permitApplicationId ? `/permit-applications/${permitApplicationId}/edit` : undefined}>
-            {permitName}
-          </RouterLink>
-        )}
+        {permitName && <RouterLink to={permitApplicationPath ?? undefined}>{permitName}</RouterLink>}
       </HStack>
       <Text variant="secondary">{format(createdAt, datefnsTableDateTimeFormat)}</Text>
     </Flex>
