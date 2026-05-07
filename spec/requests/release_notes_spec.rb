5297af5985d02fc07e0730a0e3090b0f3c98eeb5
Ian Fonberg <Ian.Fonberg@gov.bc.ca>
2026-05-07 13:47:19 -0700

    HUB-4211 add specs for release notes index

--- spec/requests/release_notes_spec.rb
+++ spec/requests/release_notes_spec.rb
@@ -110,4 +110,78 @@ RSpec.describe "ReleaseNotes", type: :request do
                             params: payload[:params]
                     end
   end
+
+  describe "#index" do
+    let!(:earliest_release_note) do
+      create(
+        :release_note,
+        status: :published,
+        version: "1.0.0",
+        release_date: 2.day.ago,
+        updated_at: 2.day.ago
+      )
+    end
+    let!(:latest_release_note) do
+      create(
+        :release_note,
+        status: :draft,
+        version: "1.0.1",
+        release_date: 1.days.ago,
+        updated_at: 1.days.ago
+      )
+    end
+
+    it "returns all release notes for super admins" do
+      sign_in super_admin
+      get release_notes_path
+
+      expect(response).to have_http_status(:success)
+      expect(subject).to have_attributes(length: 2)
+      subject.each do |release_note|
+        expect(release_note).not_to include("internal_notes")
+        expect(release_note).not_to include("debug_info")
+      end
+    end
+
+    it "returns published release notes for non-super admins" do
+      sign_in submitter
+      get release_notes_path
+
+      expect(response).to have_http_status(:success)
+      expect(subject).to have_attributes(length: 1)
+      expect(subject).to all(include("issues", "content"))
+    end
+
+    it "paginates the response" do
+      sign_in super_admin
+      get release_notes_path, params: { page: 2, per_page: 1 }
+
+      expect(response).to have_http_status(:success)
+      expect(subject).to have_attributes(length: 1)
+    end
+
+    describe "sorts", :aggregate_failures do
+      def self.it_sorts_by(field, direction, release_note)
+        it "by #{field} #{direction}" do
+          sign_in super_admin
+          get release_notes_path,
+              params: {
+                sort: {
+                  field: field,
+                  direction: direction
+                }
+              }
+
+          expect(subject.first["id"]).to eq(public_send(release_note).id)
+        end
+      end
+
+      it_sorts_by "release_date", "asc", :earliest_release_note
+      it_sorts_by "release_date", "desc", :latest_release_note
+      it_sorts_by "status", "asc", :latest_release_note
+      it_sorts_by "status", "desc", :earliest_release_note
+      it_sorts_by "updated_at", "asc", :earliest_release_note
+      it_sorts_by "updated_at", "desc", :latest_release_note
+    end
+  end
 end
