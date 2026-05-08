818cb65c3502a595952618fc75029107c76590df
Ian Fonberg <Ian.Fonberg@gov.bc.ca>
2026-05-08 15:43:50 -0700

    HUB-4211 implement release note store search functionality

---
+++ app/frontend/stores/release-note-store.ts
@@ -0,0 +1,59 @@
+import { t } from "i18next"
+import { cast, flow, Instance, types } from "mobx-state-tree"
+import { createSearchModel } from "../lib/create-search-model"
+import { withEnvironment } from "../lib/with-environment"
+import { withMerge } from "../lib/with-merge"
+import { withRootStore } from "../lib/with-root-store"
+import { IReleaseNote, ReleaseNoteModel } from "../models/release-note-model"
+import { EReleaseNoteSortFields } from "../types/enums"
+import { TSearchParams } from "../types/types"
+
+export const ReleaseNoteStoreModel = types
+  .compose(
+    types.model("ReleaseNoteStoreModel", {
+      releaseNoteMap: types.map(ReleaseNoteModel),
+      tableReleaseNotes: types.array(types.reference(ReleaseNoteModel)),
+    }),
+    createSearchModel<EReleaseNoteSortFields>("searchReleaseNotes")
+  )
+  .extend(withEnvironment())
+  .extend(withRootStore())
+  .extend(withMerge())
+  .views(() => ({
+    getSortColumnHeader(field: EReleaseNoteSortFields) {
+      // @ts-expect-error - projectAudit translation keys may not be in i18n types yet
+      return t(`releaseNote.columns.${field}`)
+    },
+  }))
+  .actions((self) => ({
+    setTableReleaseNotes(releaseNotes: IReleaseNote[]) {
+      self.tableReleaseNotes = cast(releaseNotes.map((r) => r.id))
+    },
+  }))
+  .actions((self) => ({
+    searchReleaseNotes: flow(function* (opts?: { reset?: boolean; page?: number; countPerPage?: number }) {
+      if (opts?.reset) {
+        self.resetPages()
+      }
+
+      const searchParams: TSearchParams<EReleaseNoteSortFields> = {
+        query: self.query,
+        sort: self.sort,
+        page: opts?.page ?? self.currentPage,
+        perPage: opts?.countPerPage ?? self.countPerPage,
+      }
+
+      const response = yield self.environment.api.fetchReleaseNotes(searchParams)
+
+      if (response.ok && response.data) {
+        self.mergeUpdateAll(response.data.data, "releaseNoteMap")
+        self.setTableReleaseNotes(response.data.data)
+        self.setPageFields(response.data.meta, opts)
+      } else {
+        console.error("Failed to search release notes:", response)
+      }
+      return response.ok
+    }),
+  }))
+
+export interface IReleaseNoteStore extends Instance<typeof ReleaseNoteStoreModel> {}
