3c7dd86624df9f9f9a657ad3ebb89366c82e1429
Ian Fonberg <Ian.Fonberg@gov.bc.ca>
2026-05-21 08:28:24 -0700

    HUB-4744 adjust project update messages to include state changes

--- app/presenters/project_audit_formatters/permit_project_formatter.rb
+++ app/presenters/project_audit_formatters/permit_project_formatter.rb
@@ -5,16 +5,34 @@ module ProjectAuditFormatters
       when "create"
         "#{user_display} created this project"
       when "update"
-        if changes.key?("full_address")
+        if changes.key?("state")
+          format_enum_change(
+            "project_audit.actions.changed_project_state",
+            PermitProject.states,
+            "permit_project/state",
+            "state"
+          )
+        elsif changes.key?("viewed_at")
+          format_viewed_at_change(
+            "project_audit.actions.marked_project_read",
+            "project_audit.actions.marked_project_unread"
+          ) || default_update_message
+        elsif changes.key?("full_address")
           "#{user_display} changed the project address"
         elsif changes.key?("title")
           "#{user_display} changed the project name"
         else
-          "#{user_display} updated the project"
+          default_update_message
         end
       else
         I18n.t("project_audit.fallback.generic_change", user: user_display)
       end
+    end
+
+    private
+
+    def default_update_message
+      "#{user_display} updated the project"
     end
   end
 end
