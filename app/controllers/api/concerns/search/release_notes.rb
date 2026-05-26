f0054fec6913f072c0a277a164e53f8994a547b4
Ian Fonberg <Ian.Fonberg@gov.bc.ca>
2026-05-26 14:00:48 -0700

    HUB-4214 Add year filtering to release note search functionality

--- app/controllers/api/concerns/search/release_notes.rb
+++ app/controllers/api/concerns/search/release_notes.rb
@@ -9,6 +9,7 @@ module Api::Concerns::Search::ReleaseNotes
 
   def perform_release_note_search
     relation = policy_scope(ReleaseNote)
+    relation = apply_release_note_year_filter(relation)
     relation = apply_release_note_order(relation)
     @release_notes = relation.page(release_note_page).per(release_note_per_page)
   end
@@ -16,7 +17,16 @@ module Api::Concerns::Search::ReleaseNotes
   private
 
   def release_note_search_params
-    params.permit(:page, :per_page, sort: %i[field direction])
+    params.permit(:page, :per_page, :year, sort: %i[field direction])
+  end
+
+  def apply_release_note_year_filter(relation)
+    year = release_note_search_params[:year].presence
+    return relation unless year
+
+    start_date = Date.new(year.to_i, 1, 1)
+    end_date = Date.new(year.to_i, 12, 31)
+    relation.where(release_date: start_date..end_date)
   end
 
   def release_note_page
