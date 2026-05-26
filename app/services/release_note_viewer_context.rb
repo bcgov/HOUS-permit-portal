e4ebd4f88836d366201a6da3408c536ea16ede8a
Ian Fonberg <Ian.Fonberg@gov.bc.ca>
2026-05-26 14:07:41 -0700

    HUB-4214 add release note pagination context for list view

---
+++ app/services/release_note_viewer_context.rb
@@ -0,0 +1,48 @@
+# Computes list-view pagination context for a release note so clients can open
+# /release-notes#release-note-{id} on the correct year and page.
+class ReleaseNoteViewerContext
+  def self.call(release_note:, scope:, per_page: nil)
+    new(release_note:, scope:, per_page:).call
+  end
+
+  def initialize(release_note:, scope:, per_page: nil)
+    @release_note = release_note
+    @scope = scope
+    @per_page = per_page
+  end
+
+  def call
+    year = @release_note.release_date.year
+    start_date = Date.new(year, 1, 1)
+    end_date = Date.new(year, 12, 31)
+
+    relation =
+      @scope.where(release_date: start_date..end_date).order(
+        release_date: :desc,
+        id: :desc
+      )
+
+    ahead_count =
+      relation.where(
+        "release_date > ? OR (release_date = ? AND id > ?)",
+        @release_note.release_date,
+        @release_note.release_date,
+        @release_note.id
+      ).count
+
+    {
+      release_note_id: @release_note.id,
+      year: year,
+      page: (ahead_count / resolved_per_page) + 1
+    }
+  end
+
+  private
+
+  def resolved_per_page
+    requested_per_page = @per_page.to_i
+    return requested_per_page if requested_per_page.positive?
+
+    Kaminari.config.default_per_page
+  end
+end
