98c3c32f79b3e86fdd2f98ed67fc1bde49598c40
Ian Fonberg <Ian.Fonberg@gov.bc.ca>
2026-06-02 16:56:09 -0700

    HUB-4214 re-add searchkick

--- app/models/release_note.rb
+++ app/models/release_note.rb
@@ -6,6 +6,8 @@ class ReleaseNote < ApplicationRecord
     /\A(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?\z/
   enum :status, { draft: 0, published: 1 }, default: :draft
 
+  searchkick
+
   url_validatable :release_notes_url
   validates :version,
             presence: true,
