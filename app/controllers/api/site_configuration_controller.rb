26a51e1223d905253a8c9abc0a7ca8afccf5af72
Keith M <keith@laterolabs.com>
2026-07-07 13:15:03 -0700

    Add featre flag for overheating tools

--- app/controllers/api/site_configuration_controller.rb
+++ app/controllers/api/site_configuration_controller.rb
@@ -94,6 +94,7 @@ class Api::SiteConfigurationController < Api::ApplicationController
       :allow_designated_reviewer,
       :code_compliance_enabled,
       :qa_tools_enabled,
+      :overheating_tool_enabled,
       :archistar_enabled_for_all_jurisdictions,
       help_link_items: [
         get_started_link_item: %i[href title description show],
--- spec/policies/overheating_code_policy_spec.rb
+++ spec/policies/overheating_code_policy_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe OverheatingCodePolicy do
   let(:creator_context) { UserContext.new(creator, nil) }
   let(:other_user_context) { UserContext.new(other_user, nil) }
 
+  before { SiteConfiguration.instance.update!(overheating_tool_enabled: true) }
+
   permissions :show? do
     it "allows the creator" do
       expect(policy).to permit(creator_context, overheating_code)
@@ -64,6 +66,25 @@ RSpec.describe OverheatingCodePolicy do
     end
   end
 
+  describe "when the overheating tool is disabled" do
+    before do
+      SiteConfiguration.instance.update!(overheating_tool_enabled: false)
+    end
+
+    it "denies all actions" do
+      policy_instance = described_class.new(creator_context, overheating_code)
+
+      expect(policy_instance.index?).to be(false)
+      expect(policy_instance.show?).to be(false)
+      expect(policy_instance.update?).to be(false)
+      expect(policy_instance.destroy?).to be(false)
+      expect(policy_instance.restore?).to be(false)
+      expect(
+        described_class.new(creator_context, build(:overheating_code)).create?
+      ).to be(false)
+    end
+  end
+
   describe OverheatingCodePolicy::Scope do
     it "returns only overheating codes created by the user" do
       mine = create(:overheating_code, creator: creator)
@@ -72,6 +93,15 @@ RSpec.describe OverheatingCodePolicy do
       scope = described_class.new(creator_context, OverheatingCode.all).resolve
 
       expect(scope).to contain_exactly(mine)
+    end
+
+    it "returns no records when the overheating tool is disabled" do
+      SiteConfiguration.instance.update!(overheating_tool_enabled: false)
+      create(:overheating_code, creator: creator)
+
+      scope = described_class.new(creator_context, OverheatingCode.all).resolve
+
+      expect(scope).to be_empty
     end
   end
 end
