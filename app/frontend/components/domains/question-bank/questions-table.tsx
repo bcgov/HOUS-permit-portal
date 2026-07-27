44a641ab243ef1d681d6ee15be45a2cc7cb910a5
Ian Fonberg <Ian.Fonberg@gov.bc.ca>
2026-07-27 12:05:40 -0700

    HUB-4241 use consistent row height in drawer

--- app/frontend/components/domains/question-bank/questions-table.tsx
+++ app/frontend/components/domains/question-bank/questions-table.tsx
@@ -70,133 +70,144 @@ export const QuestionsTable = observer(function QuestionsTable({
 
   return (
     <VStack as={"article"} spacing={isPicker ? 0 : 5} align="stretch" {...containerProps}>
-      <SearchGrid
-        gridRowClassName={ROW_CLASS_NAME}
-        templateColumns="repeat(6, 1fr)"
-        pos={"relative"}
+      <Box
         flex={isPicker ? 1 : undefined}
         minH={isPicker ? 0 : undefined}
-        borderRadius={isPicker ? 0 : undefined}
-        border={isPicker ? "none" : undefined}
+        overflow={isPicker ? "auto" : undefined}
+        w="full"
       >
-        <GridHeaders isPicker={isPicker} />
+        <SearchGrid
+          gridRowClassName={ROW_CLASS_NAME}
+          templateColumns="repeat(6, 1fr)"
+          pos={"relative"}
+          borderRadius={isPicker ? 0 : undefined}
+          border={isPicker ? "none" : undefined}
+          alignContent="start"
+        >
+          <GridHeaders isPicker={isPicker} />
 
-        {isSearching ? (
-          <Flex py={50} gridColumn={"span 6"}>
-            <SharedSpinner />
-          </Flex>
-        ) : (
-          tableRequirementQuestions.map((question) => {
-            const isBlocksExpanded = expandedRequirementBlockRows.has(question.id)
-            const visibleBlocks = isBlocksExpanded
-              ? question.requirementBlocks
-              : question.requirementBlocks.slice(0, PREVIEW_BLOCK_COUNT)
-            const hasMoreBlocks = question.requirementBlocks.length > PREVIEW_BLOCK_COUNT
-            const isAlreadyLinked = disabledIds.has(question.id)
+          {isSearching ? (
+            <Flex py={50} gridColumn={"span 6"}>
+              <SharedSpinner />
+            </Flex>
+          ) : (
+            tableRequirementQuestions.map((question) => {
+              const isBlocksExpanded = expandedRequirementBlockRows.has(question.id)
+              const visibleBlocks = isBlocksExpanded
+                ? question.requirementBlocks
+                : question.requirementBlocks.slice(0, PREVIEW_BLOCK_COUNT)
+              const hasMoreBlocks = question.requirementBlocks.length > PREVIEW_BLOCK_COUNT
+              const isAlreadyLinked = disabledIds.has(question.id)
 
-            return (
-              <Box key={question.id} className={ROW_CLASS_NAME} role={"row"} display={"contents"}>
-                <SearchGridItem minW="160px">
-                  <Flex direction="column" overflow="hidden">
-                    <Text as={"span"} fontWeight={700} noOfLines={2} title={question.name || undefined}>
-                      {question.name}
-                    </Text>
-                    {isPicker && question.description && (
-                      <Text as={"span"} color={"text.secondary"} fontSize={"xs"} noOfLines={2}>
+              return (
+                <Box key={question.id} className={ROW_CLASS_NAME} role={"row"} display={"contents"}>
+                  <SearchGridItem minW="160px">
+                    <Flex direction="column" overflow="hidden">
+                      <Text as={"span"} fontWeight={700} noOfLines={2} title={question.name || undefined}>
+                        {question.name}
+                      </Text>
+                      {isPicker && question.description && (
+                        <Text as={"span"} color={"text.secondary"} fontSize={"xs"} noOfLines={2}>
+                          {question.description}
+                        </Text>
+                      )}
+                    </Flex>
+                  </SearchGridItem>
+                  <SearchGridItem minW="200px">
+                    {isPicker ? (
+                      <RequirementFieldDisplay
+                        requirementType={question.inputType}
+                        label={question.label}
+                        helperText={question.hint}
+                        inputOptions={question.inputOptions}
+                        options={question.inputOptions?.valueOptions?.map((option: { label: string }) => option.label)}
+                        required
+                      />
+                    ) : (
+                      <Text as={"span"} noOfLines={3} title={question.description || undefined}>
                         {question.description}
                       </Text>
                     )}
-                  </Flex>
-                </SearchGridItem>
-                <SearchGridItem minW="200px">
-                  {isPicker ? (
-                    <RequirementFieldDisplay
-                      requirementType={question.inputType}
-                      label={question.label}
-                      helperText={question.hint}
-                      inputOptions={question.inputOptions}
-                      options={question.inputOptions?.valueOptions?.map((option: { label: string }) => option.label)}
-                      required
-                    />
-                  ) : (
-                    <Text as={"span"} noOfLines={3} title={question.description || undefined}>
-                      {question.description}
-                    </Text>
-                  )}
-                </SearchGridItem>
-                <SearchGridItem minW="120px" justifyContent="center">
-                  <HStack
-                    as={"ul"}
-                    wrap={"wrap"}
-                    spacing={1}
-                    m={0}
-                    p={0}
-                    listStyleType={"none"}
-                    justifyContent={"center"}
-                    alignItems={"center"}
-                    w={"full"}
-                  >
-                    {question.associations.map((association) => (
-                      <Tag key={association} as={"li"} bg={"greys.grey03"} color={"text.secondary"} fontSize={"xs"}>
-                        {association}
-                      </Tag>
-                    ))}
-                  </HStack>
-                </SearchGridItem>
-                <SearchGridItem pr={0} minW="180px">
-                  {question.requirementBlocks.length === 0 ? (
-                    <Text color={"text.secondary"} fontSize={"xs"}>
-                      {t("questionBank.fields.notConnected")}
-                    </Text>
-                  ) : (
-                    <UnorderedList ml={0} pl={0} w={"full"}>
-                      {visibleBlocks.map((block) => (
-                        <ListItem
-                          key={block.id}
-                          color={"text.secondary"}
-                          fontSize={"xs"}
-                          mb="1"
-                          noOfLines={1}
-                          title={block.name}
-                        >
-                          {block.name}
-                        </ListItem>
+                  </SearchGridItem>
+                  <SearchGridItem minW="120px" justifyContent="center">
+                    <HStack
+                      as={"ul"}
+                      wrap={"wrap"}
+                      spacing={1}
+                      m={0}
+                      p={0}
+                      listStyleType={"none"}
+                      justifyContent={"center"}
+                      alignItems={"center"}
+                      w={"full"}
+                    >
+                      {question.associations.map((association) => (
+                        <Tag key={association} as={"li"} bg={"greys.grey03"} color={"text.secondary"} fontSize={"xs"}>
+                          {association}
+                        </Tag>
                       ))}
-                      {hasMoreBlocks && (
-                        <Button
-                          variant={"link"}
-                          fontSize={"xs"}
-                          fontWeight={"normal"}
-                          height={"auto"}
-                          minW={"unset"}
-                          onClick={() => toggleRequirementBlocksExpanded(question.id)}
-                        >
-                          {t(isBlocksExpanded ? "questionBank.fields.seeLess" : "questionBank.fields.seeMore")}
-                        </Button>
-                      )}
-                    </UnorderedList>
-                  )}
-                </SearchGridItem>
-                <SearchGridItem minW="100px" fontSize={"sm"}>
-                  {format(question.updatedAt, datefnsTableDateFormat)}
-                </SearchGridItem>
-                <SearchGridItem justifyContent={"center"} minW="85px" flexShrink={0}>
-                  {isPicker ? (
-                    <Button size="sm" variant="primary" isDisabled={isAlreadyLinked} onClick={() => onUse?.(question)}>
-                      {t("ui.use")}
-                    </Button>
-                  ) : (
-                    <QuestionBankModal
-                      requirementQuestion={question}
-                      triggerButtonProps={{ variant: "link", size: "sm" }}
-                    />
-                  )}
-                </SearchGridItem>
-              </Box>
-            )
-          })
-        )}
-      </SearchGrid>
+                    </HStack>
+                  </SearchGridItem>
+                  <SearchGridItem pr={0} minW="180px">
+                    {question.requirementBlocks.length === 0 ? (
+                      <Text color={"text.secondary"} fontSize={"xs"}>
+                        {t("questionBank.fields.notConnected")}
+                      </Text>
+                    ) : (
+                      <UnorderedList ml={0} pl={0} w={"full"}>
+                        {visibleBlocks.map((block) => (
+                          <ListItem
+                            key={block.id}
+                            color={"text.secondary"}
+                            fontSize={"xs"}
+                            mb="1"
+                            noOfLines={1}
+                            title={block.name}
+                          >
+                            {block.name}
+                          </ListItem>
+                        ))}
+                        {hasMoreBlocks && (
+                          <Button
+                            variant={"link"}
+                            fontSize={"xs"}
+                            fontWeight={"normal"}
+                            height={"auto"}
+                            minW={"unset"}
+                            onClick={() => toggleRequirementBlocksExpanded(question.id)}
+                          >
+                            {t(isBlocksExpanded ? "questionBank.fields.seeLess" : "questionBank.fields.seeMore")}
+                          </Button>
+                        )}
+                      </UnorderedList>
+                    )}
+                  </SearchGridItem>
+                  <SearchGridItem minW="100px" fontSize={"sm"}>
+                    {format(question.updatedAt, datefnsTableDateFormat)}
+                  </SearchGridItem>
+                  <SearchGridItem justifyContent={"center"} minW="85px" flexShrink={0}>
+                    {isPicker ? (
+                      <Button
+                        size="sm"
+                        variant="primary"
+                        isDisabled={isAlreadyLinked}
+                        onClick={() => onUse?.(question)}
+                      >
+                        {t("ui.use")}
+                      </Button>
+                    ) : (
+                      <QuestionBankModal
+                        requirementQuestion={question}
+                        triggerButtonProps={{ variant: "link", size: "sm" }}
+                      />
+                    )}
+                  </SearchGridItem>
+                </Box>
+              )
+            })
+          )}
+        </SearchGrid>
+      </Box>
       <Flex
         w={"full"}
         justifyContent={"space-between"}
