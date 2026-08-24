b102b6ff8b5fe8c09e90a9074cba4619b90103ae
Ian Fonberg <Ian.Fonberg@gov.bc.ca>
2026-08-24 11:19:03 -0700

    HUB-5925 show API error text in the flash description

--- app/frontend/services/api/monitors.ts
+++ app/frontend/services/api/monitors.ts
@@ -24,7 +24,7 @@ export const addApiErrorMonitor = (api: Api, uiStore: IUIStore) => {
     if (response.problem) {
       const err = API_ERROR_TYPES[response.problem]
       if (err) {
-        uiStore.flashMessage.show(err.type, err.message, null)
+        uiStore.flashMessage.show(err.type, null, err.message)
       }
     }
   })
