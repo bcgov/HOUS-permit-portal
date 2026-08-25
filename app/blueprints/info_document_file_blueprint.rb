76b15fab2e20486809573d0d50553894b208acbe
Ian Fonberg <Ian.Fonberg@gov.bc.ca>
2026-08-25 15:44:22 -0700

    HUB-5292 add open and download urls on info document files

--- app/blueprints/info_document_file_blueprint.rb
+++ app/blueprints/info_document_file_blueprint.rb
@@ -5,4 +5,16 @@ class InfoDocumentFileBlueprint < Blueprinter::Base
 
   field :info_document_id
   field :file, transformer: Transformers::FileAttachmentTransformer
+
+  field :file_url do |file, _options|
+    file.file_url(disposition: "inline") if file.file_available?
+  rescue StandardError
+    nil
+  end
+
+  field :download_url do |file, _options|
+    file.file_url(disposition: "attachment") if file.file_available?
+  rescue StandardError
+    nil
+  end
 end
--- app/frontend/models/info-document-file.ts
+++ app/frontend/models/info-document-file.ts
@@ -5,6 +5,8 @@ export const InfoDocumentFileModel = types.model("InfoDocumentFileModel", {
   scanStatus: types.maybeNull(types.string),
   infoDocumentId: types.maybeNull(types.string),
   file: types.maybeNull(types.frozen()),
+  fileUrl: types.optional(types.maybeNull(types.string), null),
+  downloadUrl: types.optional(types.maybeNull(types.string), null),
 })
 
 export interface IInfoDocumentFile extends Instance<typeof InfoDocumentFileModel> {}
