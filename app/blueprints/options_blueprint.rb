52087a323e929d5023750e2cf145c92942d02f4e
Ian Fonberg <Ian.Fonberg@gov.bc.ca>
2026-09-11 15:20:36 -0700

    HUB-5551 include template category on for_filter options

--- app/blueprints/options_blueprint.rb
+++ app/blueprints/options_blueprint.rb
@@ -6,4 +6,16 @@ class OptionsBlueprint < Blueprinter::Base
   field :label do |record, _options|
     record.nickname
   end
+
+  field :group_id do |record, _options|
+    record.template_category_id
+  end
+
+  field :group_label do |record, _options|
+    record.template_category&.label
+  end
+
+  field :group_sort_order do |record, _options|
+    record.template_category&.sort_order
+  end
 end
--- app/controllers/api/requirement_templates_controller.rb
+++ app/controllers/api/requirement_templates_controller.rb
@@ -422,7 +422,10 @@ class Api::RequirementTemplatesController < Api::ApplicationController
       apps = apps.where(permit_project_id: project.id)
     end
 
-    RequirementTemplate.where(id: apps.select("requirement_templates.id"))
+    RequirementTemplate
+      .preload(:template_category)
+      .where(id: apps.select("requirement_templates.id"))
+      .ordered_by_template_category
   end
 
   def requirement_template_params
